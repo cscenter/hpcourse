aabea88b397cecbf5876adcae5635f45edf3f0d1
kinmanz <luga2012@yandex.ru>
2016-04-10 20:53:01 +0300

    my first

---
+++ csc/2016/myServer/Server/src/main/java/Client.java
@@ -0,0 +1,178 @@
+import java.io.*;
+import java.net.*;
+import java.util.List;
+
+import com.google.protobuf.GeneratedMessage;
+import communication.Protocol.*;
+
+public class Client {
+
+    static final int PORT_NUMBER = 44444;
+
+    public static void main(String[] args) throws InterruptedException{
+//        for (int i = 0; i < 3; i++) {
+////            Thread.sleep(1000);
+//            new TaskClient(i + " first");
+//            new TaskClient(i + " second");
+//            new TaskClient(i + " third");
+//        }
+        new TaskClient("Third", 1, "Submit");
+//        new TaskClient("Third-2", 2, "Submit");
+//        Thread.sleep(200);
+        new TaskClient("Second", 3, "Subscribe");
+//        new TaskClient("Fourth", 4, "List");
+    }
+}
+
+class TaskClient extends Thread{
+
+    String clientId;
+    long requestId;
+    Socket socket;
+    String task;
+
+    TaskClient(String clientId, long requestId, String task){
+        this.clientId = clientId;
+        this.requestId = requestId;
+        this.task = task;
+        start();
+    }
+
+    public void run(){
+        if (task.equals("Subscribe")) {
+            sendMassageAndTakeResponse(makeServerRequest(makeSubscribeTask(1)));
+        } else if (task.equals("Submit")) {
+            sendMassageAndTakeResponse(makeServerRequest(makeSubmitTask()));
+        } else {
+            sendMassageAndTakeResponse(makeServerRequest(makeListTask()));
+        }
+//        sendMassageAndTakeResponse(makeServerRequest(makeSubscribeTask(6)));
+    }
+
+    void sendMassageAndTakeResponse(ServerRequest request) {
+        try {
+            socket = new Socket("localhost", Client.PORT_NUMBER );
+
+            //Пишем
+            SendMessageToServer(request);
+            System.out.println("<<<  " + task + "  >>>");
+            // читаем ответ
+            ServerResponse serverResponse = getServerResponse();
+            System.out.println("<<<  " + "take answer" + "  >>>");
+            printResponse(serverResponse);
+        } catch (Exception e) {
+            System.out.println("work client error: ");
+            e.printStackTrace();
+            System.out.println();
+        }
+    }
+
+    void printResponse(ServerResponse serverResponse) {
+        long id = serverResponse.getRequestId();
+        System.out.println("id: " + id);
+        if (serverResponse.hasSubmitResponse()) {
+            SubmitTaskResponse submitTaskResponse = serverResponse.getSubmitResponse();
+            System.out.println("submittedTaskId: " + submitTaskResponse.getSubmittedTaskId());
+            System.out.println("status: " + submitTaskResponse.getStatus().name());
+            System.out.println();
+        } else if (serverResponse.hasSubscribeResponse()) {
+            SubscribeResponse subscribeResponse = serverResponse.getSubscribeResponse();
+            System.out.println("status: " + subscribeResponse.getStatus().name());
+            if (subscribeResponse.hasValue())
+                System.out.println("value: " + subscribeResponse.getValue());
+            System.out.println();
+        } else if(serverResponse.hasListResponse()) {
+            ListTasksResponse listTasksResponse = serverResponse.getListResponse();
+            List<ListTasksResponse.TaskDescription> listOfTasks = listTasksResponse.getTasksList();
+            System.out.println("Status:" + listTasksResponse.getStatus().name());
+            for (ListTasksResponse.TaskDescription task : listOfTasks) {
+                printTaskDescriptions(task);
+            }
+        }
+    }
+
+    void printTaskDescriptions(ListTasksResponse.TaskDescription task){
+        System.out.println("taskId: " + task.getTaskId());
+        System.out.println("clientId: " + task.getClientId());
+        if (task.hasResult()) {
+            System.out.println("Result: " + task.getResult());
+        }
+        Task curTask = task.getTask();
+        System.out.println("a: " + getParam(curTask.getA()));
+        System.out.println("b: " + getParam(curTask.getB()));
+        System.out.println("p: " + getParam(curTask.getP()));
+        System.out.println("m: " + getParam(curTask.getM()));
+        System.out.println("n: " + curTask.getN());
+    }
+
+    long getParam(Task.Param param) {
+        if (param.hasValue())
+            return param.getValue();
+        return param.getDependentTaskId();
+    }
+
+    void SendMessageToServer(ServerRequest request) throws IOException{
+        InputStream is = socket.getInputStream();
+        OutputStream os = socket.getOutputStream();
+        os.write(request.getSerializedSize());
+        request.writeTo(os);
+    }
+
+    ServerResponse getServerResponse() throws IOException {
+        int size = socket.getInputStream().read();
+        byte buf[] = new byte[size];
+        socket.getInputStream().read(buf);
+        return ServerResponse.parseFrom(buf);
+    }
+
+    //all tasks extend com.google.protobuf.GeneratedMessage
+    public ServerRequest makeServerRequest(GeneratedMessage message) {
+        ServerRequest.Builder request = ServerRequest.newBuilder().setRequestId(requestId).setClientId(clientId);
+        if (message instanceof SubmitTask) request.setSubmit((SubmitTask)message);
+        if (message instanceof Subscribe) request.setSubscribe((Subscribe)message);
+        if (message instanceof ListTasks) request.setList((ListTasks)message);
+        return request.build();
+    }
+
+    SubmitTask makeSubmitTask(){
+        SubmitTask.Builder submitTask = SubmitTask.newBuilder();
+        Task.Builder task = Task.newBuilder();
+        Task.Param.Builder param = Task.Param.newBuilder();
+
+        //параметр a
+        param.clearParamValue();
+        param.setValue(3);
+        task.setA(param.build());
+
+        //параметр b
+        param.clearParamValue();
+        param.setValue(150);
+        task.setB(param.build());
+
+        //параметр p
+        param.clearParamValue();
+        param.setValue(24);
+        task.setP(param.build());
+
+        //параметр m
+        param.clearParamValue();
+//        param.setValue(24);
+        param.setDependentTaskId(1);
+        task.setM(param.build());
+
+        //параметр n
+        task.setN(2_000_000_000l);
+
+        return submitTask.setTask(task.build()).build();
+    }
+
+    Subscribe makeSubscribeTask(int idTask){
+        return Subscribe.newBuilder().setTaskId(idTask).build();
+    }
+
+    ListTasks makeListTask() {
+        ListTasks.Builder listTask = ListTasks.newBuilder();
+        return listTask.build();
+    }
+
+}
---
+++ csc/2016/myServer/Server/src/main/java/Server.java
@@ -0,0 +1,303 @@
+import java.io.*;
+import java.lang.String;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+//import java.util.concurrent.ConcurrentHashMap;
+import util.ConcurrentHashMap;
+import com.google.protobuf.GeneratedMessage;
+
+
+import com.sun.org.apache.xpath.internal.operations.*;
+import communication.Protocol;
+import communication.Protocol.*;
+
+public class Server
+{
+    static int num = 0;
+    static final int PORT_NUMBER = 44444;
+
+    public static void main(String args[])
+    {
+        try(ServerSocket server = new ServerSocket(PORT_NUMBER);)
+        {
+            System.out.println("Server is started: ");
+
+            while(true)
+            {
+                new TaskThread(server.accept());
+                num++;
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("init server error: " + e);
+        }
+    }
+}
+
+class TaskThread extends Thread {
+    //static
+    static int initialId = 1;
+    static ConcurrentHashMap<Integer, Calculation> taskMap = new ConcurrentHashMap<>();
+//    static Map<Integer, Calculation> taskMap = new ConcurrentHashMap<Integer, Calculation>();
+    static int curId;
+
+    //no-static
+    Socket socket;
+    long requestId;
+    String clientId;
+    Calculation curTaskDescription;
+
+    TaskThread(Socket socket)
+    {
+        this.socket = socket;
+        setDaemon(true);
+        setPriority(NORM_PRIORITY);
+        start();
+        System.out.println();
+    }
+
+    public void run()
+    {
+        try
+        {
+            ServerRequest request = getServerRequest();
+            clientId = request.getClientId();
+            requestId = request.getRequestId();
+            System.out.println("clientId: " + clientId);
+            System.out.println("requestId: " + requestId);
+
+            if (request.hasSubmit()) {
+                submitHandler(request.getSubmit());
+            } else if (request.hasSubscribe()) {
+                subscribeHandler(request.getSubscribe());
+            } else if(request.hasList()) {
+                listHandler();
+            }
+
+            socket.close();
+        } catch(Exception e) {
+            System.out.print("work error: ");
+            e.printStackTrace();
+            System.out.println();
+        }
+    }
+
+    void listHandler() throws IOException{
+        ServerResponse.Builder serverResponse = ServerResponse.newBuilder();
+        serverResponse.setRequestId(requestId);
+
+        ListTasksResponse.Builder listResponse = ListTasksResponse.newBuilder().setStatus(Protocol.Status.OK);
+        listResponse.addAllTasks(getTasks());
+
+        sendToClient(serverResponse.setListResponse(listResponse.build()).build());
+    }
+
+    List<Protocol.ListTasksResponse.TaskDescription> getTasks() {
+        List<ListTasksResponse.TaskDescription> res = new LinkedList<>();
+        for (Integer taskId: taskMap.keySet()) {
+                Calculation task = taskMap.get(taskId);
+
+                ListTasksResponse.TaskDescription.Builder description =
+                        ListTasksResponse.TaskDescription.newBuilder();
+                description.setClientId(task.clientId).setTaskId(taskId).setTask(task.task);
+                if (task.isReady())
+                {
+                    description.setResult(task.getValue());
+                }
+                res.add(description.build());
+        }
+        return res;
+    }
+
+
+    void submitHandler(SubmitTask submitTask) throws IOException{
+
+        //Вытакскиваем поля
+        Task currentTask = submitTask.getTask();
+        curTaskDescription = new Calculation(requestId, clientId, currentTask);
+        curId = getNewId();
+        taskMap.put(curId, curTaskDescription);
+
+        //Тут могли встетиться ошибки тогда статус будет изменён
+        checkSubmitTaskField(currentTask.getA());
+        checkSubmitTaskField(currentTask.getB());
+        checkSubmitTaskField(currentTask.getM());
+        checkSubmitTaskField(currentTask.getP());
+
+        //Отвечаем
+//        Только в случае если поля валидны мы возвращаем статус OK
+//        И производим вычисления иначе ERROR
+        ServerResponse.Builder serverResponse = ServerResponse.newBuilder();
+        serverResponse.setRequestId(requestId);
+
+        SubmitTaskResponse.Builder submitTaskResponse = SubmitTaskResponse.newBuilder();
+        submitTaskResponse.setSubmittedTaskId(curId);
+
+
+        System.out.println("SubmitTaskResponse: ");
+        System.out.println("submittedTaskId: " + curId);
+        if (curTaskDescription.status == Status.Error) {
+            submitTaskResponse.setStatus(Protocol.Status.ERROR);
+            System.out.println("status: " + "ERROR");
+        } else {
+            submitTaskResponse.setStatus(Protocol.Status.OK);
+            System.out.println("status: " + "OK");
+        }
+
+        sendToClient(serverResponse.setSubmitResponse(submitTaskResponse.build()).build());
+
+        //Решаем задачу
+        if (curTaskDescription.status != Status.Error) {
+            long a = takeParamValue(currentTask.getA());
+            long b = takeParamValue(currentTask.getB());
+            long p = takeParamValue(currentTask.getP());
+            long m = takeParamValue(currentTask.getM());
+            long n = currentTask.getN();
+            long res = task(a, b, p, m ,n);
+            curTaskDescription.setValue(res);
+        }
+    }
+
+    void subscribeHandler(Subscribe subscribe) throws IOException {
+        int subscribeId = subscribe.getTaskId();
+        ServerResponse.Builder serverResponse = ServerResponse.newBuilder();
+        serverResponse.setRequestId(requestId);
+
+        SubscribeResponse.Builder subscribeResponse = SubscribeResponse.newBuilder();
+
+        //1) if Пытаемся подписать на задачу которой нет, или на задачу с ошибкой
+        //2) else она существует подписываемся
+        if (!taskMap.containsKey(subscribeId) || taskMap.get(subscribeId).status == Status.Error) {
+            subscribeResponse.setStatus(Protocol.Status.ERROR);
+            sendToClient(serverResponse.setSubscribeResponse(subscribeResponse).build());
+        } else {
+            long value = taskMap.get(subscribeId).getValue();
+            subscribeResponse.setStatus(Protocol.Status.OK).setValue(value);
+            sendToClient(serverResponse.setSubscribeResponse(subscribeResponse).build());
+        }
+    }
+
+    void sendToClient(GeneratedMessage message) throws IOException{
+        InputStream is = socket.getInputStream();
+        OutputStream os = socket.getOutputStream();
+        os.write(message.getSerializedSize());
+        message.writeTo(os);
+    }
+
+    //Поля уже проверены checkField function
+    long takeParamValue(Task.Param param) {
+        if (param.hasValue())
+        {
+            return param.getValue();
+        } else {
+            //(param.hasDependentTaskId())
+            return taskMap.get(param.getDependentTaskId()).getValue();
+        }
+    }
+
+    //проверка на валидность поля в случае подписки
+    void checkSubmitTaskField(Task.Param param){
+        if (param.hasDependentTaskId()) {
+            //Если мапа не содержит ключ, или содержит, но он ссылается на себя самого
+            if (param.getDependentTaskId() == curId) {
+                curTaskDescription.status = Status.Error;
+                return;
+            }
+            if  (!taskMap.containsKey(param.getDependentTaskId()) || taskMap.get(param.getDependentTaskId()).status == Status.Error) {
+                //если пытаемся взять задачу с ERROR
+                curTaskDescription.status = Status.Error;
+            }
+        } else if(!param.hasValue()) {
+            //Если и значения тоже нет
+            curTaskDescription.status = Status.Error;
+        }
+    }
+
+
+    long task(long a, long b, long p, long m, long n)
+    {
+        if (m == 0) {
+            curTaskDescription.setStatus(Status.Error);
+            return 1;
+        }
+
+        while (n-- > 0)
+        {
+            b = (a * p + b) % m;
+            a = b;
+        }
+
+        return a;
+    }
+
+    ServerRequest getServerRequest() throws IOException {
+        int size = socket.getInputStream().read();
+        byte buf[] = new byte[size];
+        socket.getInputStream().read(buf);
+        return ServerRequest.parseFrom(buf);
+    }
+
+    int getNewId() {
+        return initialId++;
+    }
+}
+//информация о каждой задачи
+class Calculation {
+
+    Status status = Status.notReady;
+    final Object GoldenBell = new Object();
+    volatile long value;
+
+    //status information
+    long requestId;
+    String clientId;
+    final Thread ownThread = Thread.currentThread();
+    final Task task;
+
+    Calculation(long requestId, String clientId, Task task) {
+        this.requestId = requestId;
+        this.clientId = clientId;
+        this.task = task;
+    }
+
+    long getValue () {
+        System.out.println("before");
+        synchronized (GoldenBell) {
+            System.out.println("after");
+            try {
+                while (!isReady()) {
+                    GoldenBell.wait();
+                }
+            } catch (InterruptedException e) {
+                System.out.println("getValue: " + e);
+            }
+            return value;
+        }
+    }
+
+    void setStatus(Status status) {
+        synchronized (GoldenBell) {
+            this.status = status;
+            if (isReady()) {
+                GoldenBell.notifyAll();
+            }
+        }
+    }
+
+    void setValue(long value) {
+        this.value = value;
+        setStatus(Status.Ready);
+    }
+
+    boolean isReady(){
+        return status == Status.Ready;
+    }
+}
+
+enum Status{
+    Ready, notReady, Error;
+}
