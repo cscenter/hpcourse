f635fbc3f52ae2a9ca1c27867c22ac6005c37b7e
Vasiliy Dommes <vasdommes@gmail.com>
2016-04-14 01:48:21 +0300

    Ready for check.
    set default port to 1234

--- csc/2016/dva/dva_1/src/main/java/server/Server.java
+++ csc/2016/dva/dva_1/src/main/java/server/Server.java
@@ -6,7 +6,7 @@ import java.io.*;
 public class Server {
     public static void main(String[] args) throws IOException {
 
-        int portNumber = 8889;
+        int portNumber = 1234;
         if (args.length == 1) {
             portNumber = Integer.parseInt(args[0]);
         }
