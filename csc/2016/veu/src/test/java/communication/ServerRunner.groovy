3996f66d4f4f5c56e3f05bf2227824677239112f
evjava <evjava@yandex.ru>
2016-04-16 01:26:48 +0300

    moved project up

--- csc/2016/veu/src/test/java/communication/ServerRunner.groovy
+++ csc/2016/veu/src/test/java/communication/ServerRunner.groovy
@@ -4,7 +4,7 @@ class ServerRunner {
   public static int port = 9703
 
   public static void main(String[] args) {
-//    System.setProperty("testEnv", "true")
+    System.setProperty("testEnv", "true")
     Server.main(port as String);
   }
 }
