9e0b3f85d1dc2a3834e320c60a3ccd6e402484be
Andrey Kokorev <a.d.kokorev@yandex.ru>
2016-04-15 15:01:45 +0300

    Client completely reworked

--- csc/2016/kad/kad_0_java/src/main/java/csc/parallel/client/Client.java
+++ csc/2016/kad/kad_0_java/src/main/java/csc/parallel/client/Client.java
@@ -1,14 +1,29 @@
 package csc.parallel.client;
 
+import communication.Protocol;
 import communication.Protocol.ServerRequest;
 import communication.Protocol.SubmitTask;
 import communication.Protocol.Task;
 import communication.Protocol.Task.Param;
 import communication.Protocol.WrapperMessage;
+import communication.Protocol.ListTasks;
+import communication.Protocol.Subscribe;
+import communication.Protocol.ListTasksResponse.TaskDescription;
 
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javafx.util.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,11 +34,16 @@ import org.slf4j.LoggerFactory;
 public class Client implements AutoCloseable
 {
     private final Logger logger = LoggerFactory.getLogger(Client.class);
+
     private final InetAddress address;
     private final int port;
-    private final Socket socket;
     private final String clientId;
-    private long requestsSent = 0;
+    private final Socket socket;
+    private AtomicLong requestsSent = new AtomicLong();
+    private Thread responseListener;
+    // requestId -> (lock, response)
+    private final Map<Long, Pair<Object, Protocol.ServerResponse>> responses = new HashMap<>();
+
     public Client(String clientId, int port) throws IOException
     {
         this(clientId, null, port);
@@ -35,35 +55,206 @@ public class Client implements AutoCloseable
         this.port = port;
         this.address = InetAddress.getByName(host);
         this.socket = new Socket(this.address, this.port);
+        startResponseListener();
+    }
+
+    private void startResponseListener()
+    {
+        responseListener = new Thread(() -> {
+            try
+            {
+                while(true)
+                {
+                    WrapperMessage response = WrapperMessage.parseDelimitedFrom(socket.getInputStream());
+                    long id = response.getResponse().getRequestId();
+                    logger.trace("parsing request {}", id);
+
+                    Object lock;
+                    synchronized (responses)
+                    {
+
+                        Pair<Object, Protocol.ServerResponse> p = responses.get(id);
+                        logger.trace("getting lock for {}", id);
+                        lock = p.getKey();
+                        responses.put(
+                                id,
+                                new Pair<>(lock, response.getResponse())
+                        );
+                    }
+
+                    synchronized (lock)
+                    {
+                        lock.notifyAll();
+                    }
+                }
+
+            } catch (IOException e)
+            {
+                logger.error(e.getMessage());
+
+                //Stubs
+                Protocol.ListTasksResponse.Builder lr = Protocol.ListTasksResponse.newBuilder()
+                        .setStatus(Protocol.Status.ERROR);
+                Protocol.SubmitTaskResponse.Builder st = Protocol.SubmitTaskResponse.newBuilder()
+                        .setSubmittedTaskId(0)
+                        .setStatus(Protocol.Status.ERROR);
+
+                Protocol.SubscribeResponse.Builder sub = Protocol.SubscribeResponse.newBuilder()
+                        .setStatus(Protocol.Status.ERROR);
+
+                //have to release everybody
+                synchronized (responses)
+                {
+                    responses.forEach((id, p) -> {
+                        Protocol.ServerResponse r = Protocol.ServerResponse.newBuilder()
+                                .setRequestId(0)
+                                .setSubmitResponse(st)
+                                .setListResponse(lr)
+                                .setSubscribeResponse(sub).build();
+                        Object lock = p.getKey();
+
+                        responses.put(id, new Pair<>(lock, r));
+                        synchronized (lock)
+                        {
+                            lock.notifyAll();
+                        }
+                    });
+                }
+
+            }
+        });
+        responseListener.setName("-- Response listener for " + this.clientId);
+        responseListener.start();
     }
 
     /**
      * Sends task using given params, returns task id
      * @throws IOException
      */
-    public int sendTask(Param a, Param b, Param p, Param m, long n) throws IOException
+    public Integer sendTask(Param a, Param b, Param p, Param m, long n) throws IOException, InterruptedException
     {
         Task.Builder t = Task.newBuilder().setA(a).setB(b).setP(p).setM(m).setN(n);
 
         SubmitTask.Builder st = SubmitTask.newBuilder().setTask(t);
-        ServerRequest.Builder r = ServerRequest.newBuilder()
-                .setClientId(this.clientId)
-                .setRequestId(requestsSent++)
-                .setSubmit(st);
 
-        WrapperMessage w = WrapperMessage.newBuilder().setRequest(r).build();
-        w.writeDelimitedTo(socket.getOutputStream());
+        long requestId = wrapAndSend(null, st, null);
 
-        WrapperMessage response = WrapperMessage.parseDelimitedFrom(socket.getInputStream());
-        int taskId = response.getResponse().getSubmitResponse().getSubmittedTaskId();
+        Protocol.ServerResponse response = waitForResponse(requestId);
+        Protocol.Status status = response.getSubmitResponse().getStatus();
+        if(status == Protocol.Status.ERROR)
+            throw new IOException("Response status: error");
 
+        int taskId = response.getSubmitResponse().getSubmittedTaskId();
         logger.debug("Task sent {}", taskId);
         return taskId;
     }
 
+    public Future<Long> subscribe(int taskId)
+    {
+        Subscribe.Builder subscribe = Subscribe.newBuilder().setTaskId(taskId);
+
+        long requestId = wrapAndSend(null, null, subscribe);
+
+        return new SubscriptionFuture(getOrCreateRequestLock(requestId), requestId);
+    }
+
+    public List<TaskDescription> listTasks() throws IOException, InterruptedException
+    {
+        long requestId = wrapAndSend(ListTasks.newBuilder(), null, null);
+        Protocol.ServerResponse response = waitForResponse(requestId);
+
+        if(response.getListResponse().getStatus() == Protocol.Status.ERROR)
+            throw new IOException("Response status: error");
+
+        return response.getListResponse().getTasksList();
+    }
+
+    private Protocol.ServerResponse waitForResponse(long requestId) throws InterruptedException
+    {
+        Object lock = getOrCreateRequestLock(requestId);
+        Protocol.ServerResponse response;
+        synchronized (lock)
+        {
+            while(true)
+            {
+                synchronized (responses)
+                {
+                    response = responses.get(requestId).getValue();
+                }
+                if(response != null)
+                    break;
+                lock.wait();
+            }
+        }
+        return response;
+    }
+
+    private Object getOrCreateRequestLock(long requestId)
+    {
+        Object lock = new Object();
+        Pair<Object, Protocol.ServerResponse> p = new Pair<>(lock, null);
+        synchronized (responses)
+        {
+            if(responses.containsKey(requestId))
+            {
+                lock = responses.get(requestId).getKey();
+            }
+            else
+            {
+                logger.trace("Creating lock for {}", requestId);
+                responses.put(requestId, p);
+            }
+        }
+        return lock;
+    }
+
+    /**
+     * Sends wrapped request, returns request id.
+     */
+    private long wrapAndSend(ListTasks.Builder list,
+                             SubmitTask.Builder submit,
+                             Subscribe.Builder subscribe)
+    {
+        long id = requestsSent.incrementAndGet();
+
+        ServerRequest.Builder r = ServerRequest.newBuilder()
+                .setClientId(this.clientId)
+                .setRequestId(id);
+
+        if(list != null)
+        {
+            r.setList(list);
+        } else if (submit != null)
+        {
+            r.setSubmit(submit);
+        } else if (subscribe != null)
+        {
+            r.setSubscribe(subscribe);
+        } else {
+            logger.error("Nothing to wrap!");
+        }
+
+        // create lock before sending
+        getOrCreateRequestLock(id);
+
+        WrapperMessage w = WrapperMessage.newBuilder().setRequest(r).build();
+        try
+        {
+            w.writeDelimitedTo(socket.getOutputStream());
+        } catch (IOException e)
+        {
+            logger.error(e.getMessage());
+        }
+
+        return id;
+    }
+
+
     @Override
     public void close()
     {
+        responseListener.interrupt();
+
         try
         {
             socket.close();
@@ -71,6 +262,78 @@ public class Client implements AutoCloseable
         {
             logger.error("Socket close error");
             logger.error(e.getMessage());
+        }
+    }
+
+    class SubscriptionFuture implements Future<Long>
+    {
+        private final Object lock;
+        private final long requestId;
+        private boolean ready = false;
+        private boolean cancelled = false;
+        private Long result;
+
+        public SubscriptionFuture(Object lock, long requestId)
+        {
+            this.lock = lock;
+            this.requestId = requestId;
+        }
+
+        @Override
+        public boolean cancel(boolean mayInterruptIfRunning)
+        {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public boolean isCancelled()
+        {
+            return cancelled;
+        }
+
+        @Override
+        public boolean isDone()
+        {
+            return ready;
+        }
+
+        @Override
+        public Long get() throws InterruptedException, ExecutionException
+        {
+            if(this.isDone())
+                return this.result;
+
+            synchronized (lock)
+            {
+                while(true){
+                    synchronized (responses)
+                    {
+                        Protocol.ServerResponse r = responses.get(requestId).getValue();
+                        if(r != null)
+                        {
+                            if(r.getSubscribeResponse().getStatus() == Protocol.Status.ERROR)
+                            {
+                                this.cancelled = true;
+                                String msg = String.format("Request %d status: error", requestId);
+                                throw new ExecutionException(msg, new IOException());
+                            }
+
+                            this.result = r.getSubscribeResponse().getValue();
+                            this.ready = true;
+                            break;
+                        }
+                    }
+                    lock.wait();
+                }
+            }
+
+            return this.result;
+        }
+
+        @Override
+        public Long get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException
+        {
+            throw new UnsupportedOperationException();
         }
     }
 }
--- csc/2016/kad/kad_0_java/src/main/java/csc/parallel/client/RandomTaskGenerator.java
+++ csc/2016/kad/kad_0_java/src/main/java/csc/parallel/client/RandomTaskGenerator.java
@@ -1,9 +1,17 @@
 package csc.parallel.client;
 
+import communication.Protocol;
 import communication.Protocol.Task.Param;
+import javafx.util.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 /**
  * @author Andrey Kokorev
@@ -11,27 +19,59 @@ import java.util.Random;
  */
 public class RandomTaskGenerator
 {
-    public static void main(String[] args) throws IOException
+    private final Logger logger = LoggerFactory.getLogger(RandomTaskGenerator.class);
+    public static void main(String[] args) throws IOException, InterruptedException
     {
         int port = 5555;
-        int clientsNum = 10;
         int tasks = 100;
+        new RandomTaskGenerator().generateAndWait("Random", port, tasks);
+    }
+
+    public long generateAndWait(String clientName, int port, int tasks) throws IOException, InterruptedException
+    {
+        List<Future<Long>> subscriptions = new ArrayList<>();
         Random r = new Random();
 
-        for(int i = 0; i < clientsNum; i++)
+        long time = System.currentTimeMillis();
+
+        Client c = new Client(clientName, port);
+
+        List<Integer> taskIds = new ArrayList<>();
+        for (int j = 0; j < tasks; j++)
         {
-            try(Client c = new Client("Client_" + i, port))
+            Param a = Param.newBuilder().setValue(r.nextInt(100000)).build();
+            Param b = Param.newBuilder().setValue(r.nextInt(100000)).build();
+            Param p = Param.newBuilder().setValue(r.nextInt(100000)).build();
+            Param m = Param.newBuilder().setValue(1 + r.nextInt(99999)).build();
+            long n = 10_000_000;
+            try
             {
-                for (int j = 0; j < tasks; j++)
-                {
-                    Param a = Param.newBuilder().setValue(r.nextInt(100000)).build();
-                    Param b = Param.newBuilder().setValue(r.nextInt(100000)).build();
-                    Param p = Param.newBuilder().setValue(r.nextInt(100000)).build();
-                    Param m = Param.newBuilder().setValue(1 + r.nextInt(99999)).build();
-                    long n = 1_000_000;
-                    c.sendTask(a, b, p, m, n);
-                }
+                Integer taskId = c.sendTask(a, b, p, m, n);
+                taskIds.add(taskId);
+            }
+            catch (IOException e)
+            {
+                logger.error("Task {} not sent", j);
             }
         }
+
+        logger.debug("Subscribing, client:{}", clientName);
+        taskIds.forEach((id) -> subscriptions.add(c.subscribe(id)));
+        logger.info("All tasks sent and subscribed, client:{}", clientName);
+
+        subscriptions.forEach((f) -> {
+            try{
+                f.get();
+            } catch (InterruptedException | ExecutionException e)
+            {
+                logger.error(e.getMessage());
+            }
+        });
+
+        long time1 = System.currentTimeMillis();
+        logger.info("client:{}, Total time {} ms", clientName, time1 - time);
+
+        c.close();
+        return time1 - time;
     }
 }
