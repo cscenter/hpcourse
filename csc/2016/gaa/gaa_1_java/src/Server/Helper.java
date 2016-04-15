9d43ca8ef65034fa57f9724e1e539ac982ff9de4
antongulikov <contest.gulikov@gmail.com>
2016-04-15 03:49:41 +0300

    add synchron map

---
+++ csc/2016/gaa/gaa_1_java/src/Server/Helper.java
@@ -0,0 +1,47 @@
+package Server;
+
+import communication.Protocol;
+import communication.Protocol.ListTasksResponse.TaskDescription;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by scorpion on 15.04.16.
+ */
+public class Helper {
+
+    private Map< Integer, TaskDescription> tasks;
+
+    private Helper() {
+        tasks = Collections.synchronizedMap(new HashMap<Integer, TaskDescription>());
+    }
+
+    public Helper getInstance() {
+        return Singelton.instance;
+    }
+
+    private static class Singelton {
+        public static Helper instance = new Helper();
+    }
+
+    public void addTask(int id, TaskDescription td) {
+        tasks.put(id, td);
+    }
+
+    public TaskDescription getTaskById(int id) {
+            return tasks.get(id);
+    }
+
+    ArrayList<TaskDescription> getAllTask() {
+        synchronized (this) {
+            ArrayList<TaskDescription> result = new ArrayList<>();
+            for (TaskDescription td : tasks.values()) {
+                result.add(td);
+            }
+            return result;
+        }
+    }
+}
