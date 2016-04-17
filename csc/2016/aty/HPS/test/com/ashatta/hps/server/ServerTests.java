9cbd8be5e692bb7b7c2e7a74c5b9f82eeb186b67
Ashatta <tatjana.agapova@gmail.com>
2016-04-17 22:24:36 +0300

    a test for independent tasks

--- csc/2016/aty/HPS/test/com/ashatta/hps/server/ServerTests.java
+++ csc/2016/aty/HPS/test/com/ashatta/hps/server/ServerTests.java
@@ -1,6 +1,10 @@
 package com.ashatta.hps.server;
 
+import static org.junit.Assert.fail;
+
 import com.ashatta.hps.communication.Protocol;
+import com.ashatta.hps.communication.Protocol.WrapperMessage;
+import javafx.util.Pair;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,8 +12,7 @@ import org.junit.Assert;
 
 import java.io.IOException;
 import java.net.Socket;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class ServerTests {
     class Client {
@@ -29,10 +32,12 @@ public class ServerTests {
     }
 
     private Server server;
+    private long requestCounter;
 
     @Before
     public void setUp() throws Exception{
         server = new Server(PortFinder.findFreePort());
+        requestCounter = 0;
         (new Thread(server)).start();
         Thread.sleep(200);
     }
@@ -44,17 +49,8 @@ public class ServerTests {
 
     @Test
     public void testSubmit() throws Exception {
-        Protocol.WrapperMessage message = Protocol.WrapperMessage.newBuilder()
-                .setRequest(Protocol.ServerRequest.newBuilder()
-                    .setClientId("SimpleClient")
-                    .setRequestId(0)
-                    .setSubmit(Protocol.SubmitTask.newBuilder()
-                        .setTask(Protocol.Task.newBuilder()
-                            .setA(buildIntParam(10))
-                            .setB(buildIntParam(12))
-                            .setP(buildIntParam(5))
-                            .setM(buildIntParam(3))
-                            .setN(5L)))).build();
+        Protocol.WrapperMessage message =
+                buildSubmitTaskMessage(buildIntParam(10), buildIntParam(12), buildIntParam(5), buildIntParam(3), 5L).getKey();
 
         Client client = new Client("localhost", server.getPort());
         client.sendWrappedMessage(message);
@@ -67,17 +63,8 @@ public class ServerTests {
 
     @Test
     public void testSubscribe() throws Exception {
-        Protocol.WrapperMessage message = Protocol.WrapperMessage.newBuilder()
-                .setRequest(Protocol.ServerRequest.newBuilder()
-                        .setClientId("SimpleClient")
-                        .setRequestId(0)
-                        .setSubmit(Protocol.SubmitTask.newBuilder()
-                                .setTask(Protocol.Task.newBuilder()
-                                        .setA(buildIntParam(10))
-                                        .setB(buildIntParam(12))
-                                        .setP(buildIntParam(5))
-                                        .setM(buildIntParam(3))
-                                        .setN(5L)))).build();
+        Protocol.WrapperMessage message =
+                buildSubmitTaskMessage(buildIntParam(10), buildIntParam(12), buildIntParam(5), buildIntParam(3), 5L).getKey();
 
         Client client = new Client("localhost", server.getPort());
         client.sendWrappedMessage(message);
@@ -85,7 +72,7 @@ public class ServerTests {
         message = Protocol.WrapperMessage.newBuilder()
                 .setRequest(Protocol.ServerRequest.newBuilder()
                         .setClientId("SimpleClient")
-                        .setRequestId(1)
+                        .setRequestId(requestCounter++)
                         .setSubscribe(Protocol.Subscribe.newBuilder()
                             .setTaskId(taskId))).build();
         client.sendWrappedMessage(message);
@@ -99,38 +86,19 @@ public class ServerTests {
 
     @Test
     public void testListTasks() throws Exception {
-        Protocol.WrapperMessage message = Protocol.WrapperMessage.newBuilder()
-                .setRequest(Protocol.ServerRequest.newBuilder()
-                        .setClientId("SimpleClient")
-                        .setRequestId(0)
-                        .setSubmit(Protocol.SubmitTask.newBuilder()
-                                .setTask(Protocol.Task.newBuilder()
-                                        .setA(buildIntParam(10))
-                                        .setB(buildIntParam(12))
-                                        .setP(buildIntParam(5))
-                                        .setM(buildIntParam(3))
-                                        .setN(5L)))).build();
+        Protocol.WrapperMessage message =
+                buildSubmitTaskMessage(buildIntParam(10), buildIntParam(12), buildIntParam(5), buildIntParam(3), 5L).getKey();
 
         Client client = new Client("localhost", server.getPort());
         client.sendWrappedMessage(message);
         int taskId1 = client.receive().getResponse().getSubmitResponse().getSubmittedTaskId();
-        message = Protocol.WrapperMessage.newBuilder()
-                .setRequest(Protocol.ServerRequest.newBuilder()
-                        .setClientId("SimpleClient")
-                        .setRequestId(1)
-                        .setSubmit(Protocol.SubmitTask.newBuilder()
-                                .setTask(Protocol.Task.newBuilder()
-                                        .setA(buildIntParam(13))
-                                        .setB(buildIntParam(12))
-                                        .setP(buildIntParam(7))
-                                        .setM(buildIntParam(8))
-                                        .setN(5L)))).build();
+        message = buildSubmitTaskMessage(buildIntParam(13), buildIntParam(12), buildIntParam(7), buildIntParam(8), 5L).getKey();
         client.sendWrappedMessage(message);
         int taskId2 = client.receive().getResponse().getSubmitResponse().getSubmittedTaskId();
         message = Protocol.WrapperMessage.newBuilder()
                 .setRequest(Protocol.ServerRequest.newBuilder()
                         .setClientId("SimpleClient")
-                        .setRequestId(2)
+                        .setRequestId(requestCounter++)
                         .setList(Protocol.ListTasks.getDefaultInstance())).build();
         client.sendWrappedMessage(message);
         Protocol.WrapperMessage responseMessage = client.receive();
@@ -145,6 +113,75 @@ public class ServerTests {
         Assert.assertEquals(2, taskIds.size());
         Assert.assertTrue(taskIds.contains(taskId1));
         Assert.assertTrue(taskIds.contains(taskId2));
+    }
+
+    @Test
+    public void testIndependentTasks() throws IOException {
+        Map<Long, Long> expectedOutputs = new HashMap<>();
+        Map<Long, Long> subscribeToSubmit = new HashMap<>();
+
+        long[] aList = { 10, 5, 39, 38, 27, 63, 25, 64, 92, 99 };
+        long[] bList = { 12, 20, 140, 2394, 2938, 12, 252, 22, 90, 59 };
+        long[] pList = { 5, 17, 123, 1, 298, 33, 299, 22, 11, 9 };
+        long[] mList = { 30, 9, 3, 198, 39, 2988, 11, 4, 21, 28 };
+        long n = 5000000;
+
+        Pair<WrapperMessage, Long> messageIdPair;
+        WrapperMessage message;
+
+        Client client = new Client("localhost", server.getPort());
+        for (int i = 0; i < aList.length; ++i) {
+            messageIdPair = buildSubmitTaskMessage(
+                    buildIntParam(aList[i]),
+                    buildIntParam(bList[i]),
+                    buildIntParam(pList[i]),
+                    buildIntParam(mList[i]),
+                    n);
+            message = messageIdPair.getKey();
+            expectedOutputs.put(messageIdPair.getValue(), compute(aList[i], bList[i], pList[i], mList[i], n));
+            client.sendWrappedMessage(message);
+        }
+
+        for (int i = 0; i < aList.length*2; ++i) {
+            Protocol.ServerResponse response = client.receive().getResponse();
+            if (response.hasSubmitResponse()) {
+                int taskId = response.getSubmitResponse().getSubmittedTaskId();
+                subscribeToSubmit.put(requestCounter, response.getRequestId());
+
+                message = Protocol.WrapperMessage.newBuilder()
+                        .setRequest(Protocol.ServerRequest.newBuilder()
+                                .setClientId("SimpleClient")
+                                .setRequestId(requestCounter++)
+                                .setSubscribe(Protocol.Subscribe.newBuilder()
+                                        .setTaskId(taskId))).build();
+                client.sendWrappedMessage(message);
+            } else if (response.hasSubscribeResponse()) {
+                Assert.assertEquals(Protocol.Status.OK, response.getSubscribeResponse().getStatus());
+                Assert.assertEquals((long) expectedOutputs.get(subscribeToSubmit.get(response.getRequestId())),
+                        response.getSubscribeResponse().getValue());
+            } else {
+                fail("Unexpected response type");
+            }
+        }
+
+    }
+
+    private Pair<Protocol.WrapperMessage, Long> buildSubmitTaskMessage(Protocol.Task.Param a, Protocol.Task.Param b,
+                                                           Protocol.Task.Param p, Protocol.Task.Param m, long n)
+    {
+        return new Pair<>(
+                Protocol.WrapperMessage.newBuilder()
+                        .setRequest(Protocol.ServerRequest.newBuilder()
+                                .setClientId("SimpleClient")
+                                .setRequestId(requestCounter++)
+                                .setSubmit(Protocol.SubmitTask.newBuilder()
+                                        .setTask(Protocol.Task.newBuilder()
+                                                .setA(a)
+                                                .setB(b)
+                                                .setP(p)
+                                                .setM(m)
+                                                .setN(n)))).build(),
+                requestCounter - 1);
     }
 
     private Protocol.Task.Param buildIntParam(long value) {
