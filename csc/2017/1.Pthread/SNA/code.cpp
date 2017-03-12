cc7b709e2d4ec0554cdda8f9f5ef40b73d745ecc
SkyForce <nikestrike0@gmail.com>
2017-03-12 21:05:02 +0300

    hw1 added

---
+++ csc/2017/1.Pthread/SNA/code.cpp
@@ -0,0 +1,124 @@
+#include <pthread.h>  
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Value {
+public:
+    Value() : _value(0) {}
+
+    void update(int value) {
+        _value = value;
+    }
+
+    int get() const {
+        return _value;
+    }
+private:
+    int _value;
+};
+
+pthread_cond_t prod, consStartCond, prodStartCond;
+bool f = false, finish = false;
+bool consStart = false;
+pthread_mutex_t m;
+
+void* producer_routine(void* arg) {
+	Value *v = (Value*) arg;
+    int t;
+	
+    vector<int> vec;
+    cin >> t;
+	while(t != 0) 
+	{
+       vec.push_back(t);
+	   cin >> t;
+    }
+	
+    pthread_mutex_lock(&m);
+    for (int i = 0; i < vec.size(); i++) {
+		
+        v->update(vec[i]);
+		
+        f = true;
+		
+        pthread_cond_broadcast(&prod);
+		
+		if(i != vec.size() - 1)
+		{
+			while(f) 
+			{
+				pthread_cond_wait(&prod, &m);
+			}
+		}
+    }
+	finish = true;
+    pthread_mutex_unlock(&m);
+
+    pthread_exit(NULL);
+
+}
+
+void* consumer_routine(void* arg) {
+	pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
+    Value *v = (Value*)arg;
+
+    pthread_mutex_lock(&m);
+	
+    consStart = true;
+	
+    pthread_cond_broadcast(&consStartCond);
+	int* res = new int;
+	*res = 0;
+    while (true) {
+        while (!f && !finish) 
+		{
+            pthread_cond_wait(&prod, &m);
+        }
+		
+        *res += v->get();
+		
+        f = false;
+        pthread_cond_broadcast(&prod);
+
+        if (finish) 
+		{
+			pthread_mutex_unlock(&m);
+            pthread_exit(res);
+        }
+    }
+	
+}
+
+void* consumer_interruptor_routine(void* arg) {
+	pthread_mutex_lock(&m);
+    while(!consStart) 
+	{
+        pthread_cond_wait(&consStartCond, &m);
+    }
+    pthread_mutex_unlock(&m);
+
+    while (!pthread_cancel(*(pthread_t*)(arg))) 
+	{
+	}
+    pthread_exit(NULL);
+}
+
+int run_threads() {
+    pthread_t thread1, thread2, thread3;
+	void* res;
+    Value v;
+    pthread_create(&thread1, NULL, producer_routine, (void *)&v);
+    pthread_create(&thread2, NULL, consumer_routine, (void *)&v);
+    pthread_create(&thread3, NULL, consumer_interruptor_routine, (void *)&thread2);
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, &res);
+    pthread_join(thread3, NULL);
+	
+    return *((int*) res);
+}
+
+int main() {
+    std::cout << run_threads() << std::endl;
+    return 0;
+}
