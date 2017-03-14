9535ccc1f8b866089ed53e66cd6196e2028dad0e
Lena Volzhina <lenavolzhina@ya.ru>
2017-03-14 23:31:07 +0300

    replaced flags with condition variables

--- csc/2017/1.Pthread/veg/Task.cpp
+++ csc/2017/1.Pthread/veg/Task.cpp
@@ -1,5 +1,6 @@
-#include <pthread.h>  
 #include <iostream>
+#include <pthread.h>
+
 
 class Value {
 public:
@@ -17,27 +18,83 @@ private:
     int _value;
 };
 
+pthread_mutex_t data_lock;
+// maybe I should've add mutex for each flag, but now I don't really need it
+
+pthread_cond_t consumer_started_cond;
+pthread_cond_t data_updated_cond;
+pthread_cond_t data_ended_cond;
+
 bool consumer_started = false;
 bool data_updated = false;
 bool data_ended = false;
 
+void set_data_updated(bool value) {
+    pthread_mutex_lock(&data_lock);
+    data_updated = value;
+    pthread_cond_signal(&data_updated_cond);
+    pthread_mutex_unlock(&data_lock);
+}
+
+void set_data_ended(bool value) {
+    pthread_mutex_lock(&data_lock);
+    data_ended = value;
+    pthread_cond_signal(&data_ended_cond);
+    pthread_mutex_unlock(&data_lock);
+}
+
+void set_consumer_started(bool value) {
+    pthread_mutex_lock(&data_lock);
+    consumer_started = value;
+    pthread_cond_broadcast(&consumer_started_cond);
+    pthread_mutex_unlock(&data_lock);
+}
+
+void wait_for_data_updated(bool value) {
+    pthread_mutex_lock(&data_lock);
+    while (data_updated != value) {
+        pthread_cond_wait(&data_updated_cond, &data_lock);
+    }
+    pthread_mutex_unlock(&data_lock);
+}
+
+void wait_for_consumer_started(bool value) {
+    pthread_mutex_lock(&data_lock);
+    while (consumer_started != value) {
+        pthread_cond_wait(&consumer_started_cond, &data_lock);
+    }
+    pthread_mutex_unlock(&data_lock);
+}
 
 void *producer_routine(void *arg) {
     Value *data = (Value *) arg;
 
     // wait for consumer to start
-    while (!consumer_started);
+    wait_for_consumer_started(true);
 
-    int n;
     // read data, loop through each value
+    int n;
     while (std::cin >> n) {
-        while (data_updated);      // wait for consumer to process previous value
-        data->update(n);           // update value
-        data_updated = true;       // notify consumer
+        // wait for consumer to process previous value
+        wait_for_data_updated(false);
+
+        // lock mutex to modify data
+        pthread_mutex_lock(&data_lock);
+
+        // update value and unlock mutex
+        data->update(n);
+        pthread_mutex_unlock(&data_lock);
+
+        // notify consumer
+        set_data_updated(true);
     }
 
-    while (data_updated);     // make sure last value is processed
-    data_ended = true;
+    // make sure last value is processed (and consumer put data_updated=false)
+    wait_for_data_updated(false);
+
+    // notify everyone that data ended
+    set_data_ended(true);
+    set_data_updated(true);  // not elegant, but consumer waits for this signal
 
     pthread_exit(NULL);
 }
@@ -49,22 +106,20 @@ void *consumer_routine(void *arg) {
     Value *data = (Value *) arg;
 
     // notify about start
-    consumer_started = true;
+    set_consumer_started(true);
 
     // allocate value for result
     int *result = new int(0);
 
     // for every update issued by producer, read the value and add to sum
-    while (true) {
-        while (!data_updated & !data_ended);      // wait for producer to read new value
+    while (!data_ended) {
+        wait_for_data_updated(true);  // wait for producer to read new value
 
         if (data_ended) break;
 
         *result += data->get();
-        data_updated = false;
+        set_data_updated(false);
     }
-
-    pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
 
     // return pointer to result
     pthread_exit((void **) result);
@@ -74,7 +129,7 @@ void *consumer_interrupter_routine(void *arg) {
     pthread_t *consumer = (pthread_t *) arg;
 
     // wait for consumer to start
-    while (!consumer_started);
+    wait_for_consumer_started(true);
 
     // interrupt consumer while producer is running
     while (!data_ended & !pthread_cancel(*consumer));
