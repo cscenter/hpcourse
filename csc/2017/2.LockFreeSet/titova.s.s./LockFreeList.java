db8adfb81f4d0cde2dba2868f35f4e8abf0063fa
Titova Sophia <sof_titova@rambler.ru>
2017-04-17 15:07:02 +0300

    Update LockFreeList.java

--- csc/2017/2.LockFreeSet/titova.s.s./LockFreeList.java
+++ csc/2017/2.LockFreeSet/titova.s.s./LockFreeList.java
@@ -1,177 +1,149 @@
 import javafx.util.Pair;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicMarkableReference;
 
 /**
  * Created by Sophia Titova on 06.04.17.
  */
 public class LockFreeList<T extends Comparable<T>> implements LockFreeSet<T> {
-    Node emptyNode = new Node();
-    private AtomicMarkableReference<Node> head = new AtomicMarkableReference<>(emptyNode, false);
-    private AtomicInteger size = new AtomicInteger(0);
-    
-    public static void main(String[] args) {
-        
-        LockFreeList<Integer> integerLockFreeList = new LockFreeList<>();
-        
-        integerLockFreeList.add(1);
-        integerLockFreeList.add(2);
-        integerLockFreeList.add(2);
-        integerLockFreeList.remove(1);
-        
-        System.out.println(integerLockFreeList.contains(1));
-        
-        integerLockFreeList.remove(2);
-        
-        System.out.println(integerLockFreeList.contains(2));
+    private Node HEAD;
+    private Node TAIL;
+
+
+    public LockFreeList() {
+        this.HEAD = new Node(null);
+        this.TAIL = new Node(null);
+        this.HEAD.next.set(this.TAIL, false);
     }
-    
+
+    private boolean checkNotDeleted(Node first, Node second) {
+        return !first.getDeleted() && !second.getDeleted() && first.getNext().getReference() == second;
+    }
+
     @Override
     public boolean add(T value) {
-        
-        Node pred;
+
+        Node prev;
         Node curr;
-        Node next;
         Pair<Node, Pair<Node, Node>> p;
-        
         while (true) {
-            
             p = find(value);
-            pred = p.getKey();
+            prev = p.getKey();
             curr = p.getValue().getKey();
-            next = p.getValue().getValue();
-            
-            if (curr != null) {
-                return false;
-            }
-            
-            Node node = new Node(value);
-            node.setNext(new AtomicMarkableReference<>(next, false));
-            
-            if (size.get() == 0) {
-                head.compareAndSet(emptyNode, node, false, false);
-                size.compareAndSet(0, 1);
-                return true;
-            }
-            
-            if (pred.getNext().compareAndSet(next, node, false, false)) {   // а другие сслки перевесить? ходим в одну сторону телько видимо
-                size.incrementAndGet();
-                return true;
-            }
-        }
-    }
-    
-    @Override
-    public boolean remove(T value) {
-        
-        Node pred;
-        Node curr;
-        Node next;
-        Pair<Node, Pair<Node, Node>> p;
-        
-        while (true) {
-            
-            p = find(value);
-            pred = p.getKey();
-            curr = p.getValue().getKey();
-            next = p.getValue().getValue();
-            
-            if (size.get() == 1) {
-                if (pred.getValue() == value) {
-                    size.compareAndSet(1, 0);
+            if (checkNotDeleted(prev, curr)) {
+                if (curr.getValue() != null && curr.getValue().compareTo(value) == 0) {
+                    return false;
+                }
+                Node node = new Node(value);
+                node.setNext(new AtomicMarkableReference<>(curr, false));
+                if (prev.getNext().compareAndSet(curr, node, false, false)) {
                     return true;
                 }
             }
-            if (curr == null) {
-                return false;
-            }
-            
-            if (!curr.getNext().attemptMark(next, true)) {
-                continue;
-            }
-            
-            pred.getNext().compareAndSet(curr, next, false, false);
-            size.decrementAndGet();
-            return true;
         }
     }
-    
-    
+
+    @Override
+    public boolean remove(T value) {
+        Node prev;
+        Node curr;
+        Node next;
+        Pair<Node, Pair<Node, Node>> p;
+        while (true) {
+            p = find(value);
+            prev = p.getKey();
+            curr = p.getValue().getKey();
+            next = p.getValue().getValue();
+            if (checkNotDeleted(prev, curr)) {
+                if (curr.getValue() == null || curr.getValue().compareTo(value) != 0) {
+                    return false;
+                }
+                curr.setDeleted(true);
+                if (!curr.getNext().attemptMark(next, true)) {
+                    continue;
+                }
+                prev.getNext().compareAndSet(curr, next, false, false);
+                return true;
+            }
+        }
+    }
+
+
     @Override
     public boolean contains(T value) {
-        while (true) {
-            return find(value).getValue().getKey() != null;
+        Node curr = HEAD.getNext().getReference();
+        while (curr.getValue() != null && curr.getValue().compareTo(value) != 0) {
+            curr = curr.getNext().getReference();
         }
+        return curr.getValue() != null && !curr.getDeleted() && curr.getValue().compareTo(value) == 0;
     }
-    
+
     @Override
     public boolean isEmpty() {
-        return size.get() == 0;
+        return this.HEAD.getNext().getReference() == this.TAIL;
     }
-    
-    private Pair<Node, Pair<Node, Node>> find(Comparable value) { // TODO
+
+    private Pair<Node, Pair<Node, Node>> find(T value) { 
         Node pred;
         Node curr;
         Node next;
-        boolean[] pMark = new boolean[1];
         boolean[] cMark = new boolean[1];
-        int key = value.hashCode();
-    again:
+        cMark[0] = false;
+        again:
         while (true) {
-            
-            if (size.get() == 0) {
-                return new Pair<>(null, new Pair<>(null, null));
-            }
-            pred = head.getReference();
-            curr = pred.getNext().get(pMark);
+            pred = HEAD;
+            curr = HEAD.next.getReference();
             while (true) {
-                if (curr == null) {
-                    return new Pair<>(pred, new Pair<>(null, null));
-                }
                 next = curr.getNext().get(cMark);
-                int ckey = curr.hashCode();
                 if (pred.getNext().isMarked()) {
-                    continue again;     // changed
+                    continue again;
                 }
-                if (!cMark[0]) {
-                    if (curr.getValue().equals(value)) {
-                        return new Pair<>(pred, new Pair<>(curr, next));
-                    } else if (ckey <= key) {
-                        pred = curr;
-                    } else {
-                        return new Pair<>(pred, new Pair<>(null, next));
-                    }
-                } else {
+                while (cMark[0]) {
                     if (!pred.getNext().compareAndSet(curr, next, false, false)) {
                         continue again;
                     }
+                    curr = pred.getNext().getReference();
+                    next = curr.getNext().get(cMark);
                 }
+                if (curr.getValue() == null || curr.getValue().compareTo(value) == 0) {
+                    return new Pair<>(pred, new Pair<>(curr, next));
+                }
+                pred = curr;
+                curr = next;
             }
         }
     }
-    
-    
+
+
     private class Node {
-        
+
+        private AtomicBoolean deleted = new AtomicBoolean();
         private AtomicMarkableReference<Node> next;
         private T value;
-        
-        Node() {
-        }
-        
+
         Node(T value) {
+            deleted.set(false);
             this.value = value;
+            next = new AtomicMarkableReference<Node>(null, false);
         }
-        
+
         T getValue() {
             return value;
         }
-        
+
+        boolean getDeleted() {
+            return deleted.get();
+        }
+
+        void setDeleted(boolean b) {
+            deleted.set(b);
+        }
+
         AtomicMarkableReference<Node> getNext() {
             return next;
         }
-        
+
         void setNext(AtomicMarkableReference<Node> next) {
             this.next = next;
         }
