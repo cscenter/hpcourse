6935e75502903e81c5714648cc4884770b8418dc
anastasia143 <semenova.anastasia143@yandex.ru>
2017-05-05 17:54:09 +0300

    Second version of hw2

--- csc/2017/2.LockFreeSet/semenova_anastasiia/LockFreeSetImpl.java
+++ csc/2017/2.LockFreeSet/semenova_anastasiia/LockFreeSetImpl.java
@@ -1,159 +1,120 @@
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicMarkableReference;
 
+public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> {
 
-public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet {
-    private AtomicMarkableReference<Node> head = new AtomicMarkableReference<>(null, false);
-    private AtomicInteger size = new AtomicInteger(0);
+    private final Node head = new Node(null, (new Node(null, null)));
 
     private class Node {
-        private AtomicMarkableReference<Node> next;
-        private Comparable value;
+        public AtomicMarkableReference<Node> next;
+        public Comparable value;
 
-        Node(final Comparable value, final AtomicMarkableReference<Node> next) {
+        public Node(final Comparable value, final Node next) {
             this.value = value;
-            this.next = next;
+            this.next = new AtomicMarkableReference<>(next, false);
+        }
+    }
+
+    private class SearchResult {
+        final Node previous;
+        final Node current;
+
+        SearchResult(final Node previous, final Node current) {
+            this.previous = previous;
+            this.current = current;
         }
     }
 
     @Override
-    public boolean add(Comparable value) {
+    public boolean add(final Comparable value) {
         while (true) {
-            // 1. If list is empty, set new head value
-            if (head.getReference() == null) {
-                Node newNode = new Node(value, new AtomicMarkableReference<>(null, false));
+            final SearchResult searchResult = find(value);
 
-                if (head.compareAndSet(null, newNode, false, false)) {
-                    size.incrementAndGet();
+            if (searchResult.current.value == null || value.compareTo(searchResult.current.value) != 0) {
+                Node node = new Node(value, searchResult.current);
+
+                // Trying to add created element
+                if (searchResult.previous.next.compareAndSet(searchResult.current, node, false, false)) {
                     return true;
-                } else {
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+
+    @Override
+    public boolean remove(final Comparable value) {
+        while (true) {
+            final SearchResult searchResult = find(value);
+
+            if (searchResult.current.value != null && value.compareTo(searchResult.current.value) == 0) {
+
+                final Node next = searchResult.current.next.getReference();
+
+                // Trying to mark element as read
+                if (!searchResult.current.next.attemptMark(next, true)) {
                     continue;
                 }
-            }
 
-            // 2. If list contains the value and it is not deleted, return false
-            Node current = head.getReference();
-            boolean currentIsMarked = head.isMarked();
+                // Trying to remove element physically
+                // Not interested in the result, could delete this node during the next search
+                searchResult.previous.next.compareAndSet(searchResult.current, next, false, false);
 
-            while (current != null) {
-                if (value.compareTo(current.value) == 0 && !currentIsMarked) {
-                    return false;
-                }
-
-                currentIsMarked = current.next.isMarked();
-                current = current.next.getReference();
-            }
-
-            // 3. Otherwise set head value
-            Node oldHead = head.getReference();
-            boolean headIsMarked = head.isMarked();
-            Node newNode = new Node(value, new AtomicMarkableReference<>(oldHead, headIsMarked));
-
-
-            if (head.compareAndSet(oldHead, newNode, headIsMarked, false)) {
-                size.incrementAndGet();
                 return true;
-            }
-        }
-
-    }
-
-    @Override
-    public boolean remove(Comparable value) {
-        while (true) {
-            // 1. If list is empty, we couldn't remove anything
-            if (head.getReference() == null) {
+            } else {
                 return false;
             }
-
-            // 2. If not, we will try to find the value.
-            // If we find it, mark it as read.
-            AtomicMarkableReference<Node> current = head;
-            Node currentReference = current.getReference();
-            boolean currentIsMarked = current.isMarked();
-            boolean casFailed = false;
-            while (currentReference != null) {
-
-                if (value.compareTo(currentReference.value) == 0 && !currentIsMarked) {
-                    if (current.compareAndSet(currentReference, currentReference, false, true)) {
-                        size.decrementAndGet();
-                        return true;
-                    } else {
-                        casFailed = true;
-                        // from first loop
-                        continue;
-                    }
-                }
-                current = current.getReference().next;
-                currentReference = current.getReference();
-                currentIsMarked = current.isMarked();
-            }
-
-            if (casFailed) {
-                // from second loop
-                continue;
-            }
-
-            // 3. if we fail to find the value, return false
-            return false;
         }
     }
 
+    // Don't call find. Contains should be wait-free
     @Override
-    public boolean contains(Comparable value) {
-        while (true) {
-            // 1. If list is empty, it doesn't contain anything
-            if (head.getReference() == null) {
-                return false;
-            }
+    public boolean contains(final Comparable value) {
+        boolean[] isDeleted = {false};
+        Node current = head.next.get(isDeleted);
 
-            // 2. If not, we will try to find the value.
-            // If we find it, then return true.
-            // Let's clean up removed values during the search.
-            AtomicMarkableReference<Node> current = head;
-            Node previous = head.getReference();
-            Node currentReference = current.getReference();
-            boolean currentIsMarked = current.isMarked();
-
-            while (currentReference != null) {
-                if (value.compareTo(currentReference.value) == 0 && !currentIsMarked) {
-                    return true;
-                }
-
-                if (currentIsMarked) {
-                    boolean nextIsMarked = currentReference.next.isMarked();
-
-                    if (currentReference.equals(head.getReference())) {
-                        if (head.compareAndSet(currentReference, currentReference.next.getReference(), currentIsMarked, nextIsMarked)) {
-                            previous = head.getReference();
-                            current = head;
-                        } else {
-                            previous = current.getReference();
-                            current = current.getReference().next;
-                        }
-                    } else {
-                        if (previous.next.compareAndSet(currentReference, currentReference.next.getReference(), currentIsMarked, nextIsMarked)) {
-                            current = previous.next;
-                        } else {
-                            previous = current.getReference();
-                            current = current.getReference().next;
-                        }
-                    }
-                } else {
-                    previous = current.getReference();
-                    current = current.getReference().next;
-                }
-                currentReference = current.getReference();
-                currentIsMarked = current.isMarked();
-            }
-
-            // 3. if we fail to find the value, return false.
-            return false;
+        while (current.value != null && value.compareTo(current.value) > 0) {
+            current = current.next.get(isDeleted);
         }
+
+        return current.value != null && value.compareTo(current.value) == 0 && !isDeleted[0];
     }
 
     @Override
     public boolean isEmpty() {
-        return (size.get() == 0);
+        return head.next.getReference().value == null;
     }
+
+    public SearchResult find(final Comparable value) {
+        tryAgain:
+
+        while (true) {
+
+            Node previous = head;
+            Node current = head.next.getReference();
+
+            while (true) {
+                boolean[] isDeleted = {false};
+                Node next = current.next.get(isDeleted);
+
+                // Trying to delete marked element physically
+                while (isDeleted[0]) {
+                    if (!previous.next.compareAndSet(current, next, false, false)) {
+                        continue tryAgain;
+                    }
+                    current = next;
+                    next = current.next.get(isDeleted);
+                }
+
+                // Getting element if it is the right place for searched element
+                if (current.value == null || value.compareTo(current.value) <= 0) {
+                    return new SearchResult(previous, current);
+                }
+
+                previous = current;
+                current = next;
+            }
+        }
+    }
+
 }
