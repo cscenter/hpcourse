f6b9229feb07490550b7655a84b097bbabfeb56a
Alica032 <alicadr.032@gmail.com>
2017-05-11 21:27:20 +0300

    Update LockFreeSetImpl.java

--- csc/2017/2.LockFreeSet/fse/LockFreeSetImpl.java
+++ csc/2017/2.LockFreeSet/fse/LockFreeSetImpl.java
@@ -1,11 +1,128 @@
+
 import java.util.concurrent.atomic.AtomicMarkableReference;
 
 public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> {
 
-    private final AtomicMarkableReference<Node> tail =
+    final private Node tail = new Node(null);
+    final private Node head = new Node(null, tail,false);
+
+    SearchResult search(T search_key) {
+        retry: while (true) {
+            Node left = head;
+            Node right = head.next.getReference();
+            Node succ;
+            boolean[] t = new boolean[1];
+                while (true){
+                    succ = right.next.get(t);
+                    boolean cmk = t[0];
+                    if(cmk){
+                        if(!left.next.compareAndSet(right, succ, false,false))
+                            continue retry;
+                        right = succ;
+                    }
+                    else {
+                        if(right == tail || right.item.compareTo(search_key) >= 0)
+                            return new SearchResult(left, right);
+                        left = right;
+                        right = succ;
+                    }
+                }
+            }
+    }
+
+    public boolean add(T key) {
+        Node right_node, left_node;
+        do {
+            SearchResult res = search(key);
+            left_node = res.left;
+            right_node = res.right;
+
+            if ((right_node != tail) && (right_node.item.compareTo(key)) == 0)
+                return false;
+            Node new_node = new Node(key,right_node,false);
+            if (left_node.next.compareAndSet(right_node, new_node, false, false))
+                return true;
+        } while (true);
+    }
+
+    public boolean remove(T search_key) {
+        Node right_node, right_node_next, left_node;
+        do {
+            SearchResult res = search(search_key);
+            left_node = res.left;
+            right_node = res.right;
+
+            if ((right_node == tail) ||
+                    (right_node.item.compareTo(search_key)) != 0)
+                return false;
+
+            right_node_next = right_node.next.getReference();
+            if (!right_node.next.isMarked())
+                if (right_node.next.compareAndSet(right_node_next, right_node_next, false, true))
+                    break;
+        } while (true);
+       
+        if (!left_node.next.compareAndSet(right_node, right_node_next, false, false))
+            search(right_node.item);
+        return true;
+    }
+
+    public boolean contains(T search_key) {
+
+        Node right_node = head;
+        do{
+            right_node = right_node.next.getReference();
+        }while (right_node != tail && right_node.item.compareTo(search_key) < 0);
+
+        return right_node!= tail &&
+                right_node.item.compareTo(search_key) == 0 &&
+                !right_node.next.isMarked();
+
+    }
+
+
+    public boolean isEmpty() {
+        return head.next.getReference() == tail;
+    }
+
+    private class Node {
+        final T item;
+        final AtomicMarkableReference<Node> next;
+
+        public Node(T item) {
+            this.item=item;
+            this.next=new AtomicMarkableReference<>(null,false);
+        }
+
+        public Node(T item, Node nextNode, boolean mark) {
+            this.item=item;
+            this.next=new AtomicMarkableReference<>(nextNode,mark);
+        }
+    }
+
+    private class SearchResult{
+        final Node left, right;
+
+        SearchResult(Node left, Node right) {
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+
+
+/**
+version 2
+
+
+import java.util.concurrent.atomic.AtomicMarkableReference;
+
+public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> {
+
+    final private AtomicMarkableReference<Node> tail =
             new AtomicMarkableReference(new Node(null), false);
 
-    private final AtomicMarkableReference<Node> head
+    final private AtomicMarkableReference<Node> head
             = new AtomicMarkableReference(
             new Node(null,tail.getReference(),false),
             false
@@ -15,7 +132,6 @@ public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> 
         AtomicMarkableReference<Node> left_node = new AtomicMarkableReference<>(null,false);
         AtomicMarkableReference<Node> right_node;
         AtomicMarkableReference<Node> left_node_next = new AtomicMarkableReference<>(null,false);
-        search_again:
         while (true) {
             AtomicMarkableReference<Node> t = head;
             AtomicMarkableReference<Node> t_next = head.getReference().next;
@@ -34,19 +150,18 @@ public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> 
 
             if (left_node_next.getReference() == right_node.getReference())
                 if ((right_node.getReference() != tail.getReference()) && right_node.getReference().next.isMarked())
-                    break search_again;
+                    continue;
                 else {
                     return new SearchResult(left_node,right_node);
                 }
 
             if (left_node.getReference().next.compareAndSet(left_node_next.getReference(), right_node.getReference(), left_node.getReference().next.isMarked(), left_node.getReference().next.isMarked()))
                 if ((right_node.getReference() != tail.getReference()) && right_node.getReference().next.isMarked())
-                    break search_again;
+                    continue;
                 else {
                     return new SearchResult(left_node,right_node);
                 }
         }
-        return null;
     }
 
 
@@ -55,8 +170,9 @@ public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> 
         AtomicMarkableReference<Node> right_node, left_node;
         do {
             SearchResult res = search(key);
-            right_node = res.right;
             left_node = res.left;
+            right_node = res.right;
+
             if ((right_node.getReference() != tail.getReference()) && (right_node.getReference().item.compareTo(key)) == 0)
                 return false;
             new_node.getReference().next = right_node;
@@ -70,8 +186,9 @@ public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> 
         AtomicMarkableReference<Node> right_node, right_node_next, left_node;
         do {
             SearchResult res = search(search_key);
-            right_node = res.right;
             left_node = res.left;
+            right_node = res.right;
+
 
             if ((right_node.getReference() == tail.getReference()) ||
                     (right_node.getReference().item.compareTo(search_key)) != 0)
@@ -123,7 +240,7 @@ public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> 
     }
 
     private class SearchResult{
-        AtomicMarkableReference<Node> left, right;
+        final AtomicMarkableReference<Node> left, right;
 
         SearchResult(AtomicMarkableReference<Node> left, AtomicMarkableReference<Node> right) {
             this.left = left;
@@ -131,3 +248,5 @@ public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> 
         }
     }
 }
+
+*/
