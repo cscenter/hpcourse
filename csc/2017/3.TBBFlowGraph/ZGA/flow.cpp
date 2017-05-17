429c8ccb9bb0011fce55182ce1bb2c3dae1814dc
greg <lahmatbiy@gmail.com>
2017-05-17 13:47:40 +0300

    Hell yeah!! it works
    Signed-off-by: greg <lahmatbiy@gmail.com>

--- csc/2017/3.TBBFlowGraph/ZGA/flow.cpp
+++ csc/2017/3.TBBFlowGraph/ZGA/flow.cpp
@@ -9,6 +9,7 @@
 using namespace std;
 using namespace tbb::flow;
 
+const string pathToBigImg = "data/image.dat";
 struct pixel {
 	uint8_t r;
 	uint8_t g;
@@ -17,28 +18,36 @@ struct pixel {
 
 
 int differenceBetween(const pixel& lhs, const pixel& rhs) {
-	return abs(lhs.r - rhs.r
-			   + lhs.b - rhs.b
-			   + lhs.g - rhs.g);
+	// uint -> int -> abs
+	return abs((int)lhs.r - (int)rhs.r)
+			+ abs((int)lhs.b - (int)rhs.b)
+			+ abs((int)lhs.g - (int)rhs.g);
 }
 
 struct rect {
-	rect(uint16_t tx, uint16_t ty, uint16_t bx, uint16_t by) {
-		topLeftX = tx;
-		topLeftY = ty;
-		bottomRightX = bx;
-		bottomRightY = by;
+	rect(uint16_t th, uint16_t tw, uint16_t bh, uint16_t bw) {
+		topLeftH = th;
+		topLeftW = tw;
+		bottomRightH = bh;
+		bottomRightW = bw;
 	}
 
-	uint16_t topLeftX = 0;
-	uint16_t topLeftY = 0;
-	uint16_t bottomRightX = 0;
-	uint16_t bottomRightY = 0;
+	rect(){}
+
+	friend std::ostream& operator<< (std::ostream& stream, const rect& r) {
+		stream << r.topLeftH << " " << r.topLeftW << " " << r.bottomRightH << " " << r.bottomRightW << "\n";
+		return stream;
+	}
+
+	uint16_t topLeftH = 0;
+	uint16_t topLeftW = 0;
+	uint16_t bottomRightH = 0;
+	uint16_t bottomRightW = 0;
 };
 
 inline bool operator==(const rect& lhs, const rect& rhs) {
-	return lhs.bottomRightX == rhs.bottomRightX && lhs.bottomRightY == rhs.bottomRightY
-			&& lhs.topLeftX == rhs.topLeftX && lhs.topLeftY == rhs.topLeftY;
+	return lhs.bottomRightH == rhs.bottomRightH && lhs.bottomRightW == rhs.bottomRightW
+			&& lhs.topLeftW == rhs.topLeftW && lhs.topLeftW == rhs.topLeftW;
 }
 
 
@@ -96,15 +105,16 @@ void imwrite(const image& source, const string& path) {
 }
 
 image getRegionImg(const image &src, const rect &dstRect) {
-	size_t h = dstRect.bottomRightX - dstRect.topLeftX + 1;
-	size_t w = dstRect.bottomRightX - dstRect.topLeftX + 1;
+	size_t h = dstRect.bottomRightH - dstRect.topLeftH + 1;
+	size_t w = dstRect.bottomRightW - dstRect.topLeftW + 1;
+	cout << h << " " << w << " rect: " << dstRect;
 	auto dstImg = vector<vector<pixel>>(h);
 	for (auto &row : dstImg)
 		row.resize(w);
 
 	for (size_t i = 0; i < h; ++i) {
 		for (size_t j = 0; j < w; ++j) {
-			dstImg[i][j] = src[i + dstRect.topLeftX][j + dstRect.topLeftY];
+			dstImg[i][j] = src[i + dstRect.topLeftH][j + dstRect.topLeftW];
 		}
 	}
 
@@ -117,7 +127,6 @@ class imageSlicer {
 	image mBigImage;
 public:
 	imageSlicer(const string &pathToSmallImg) {
-		const string pathToBigImg = "data/image.dat";
 		mBigImage = imread(pathToBigImg);
 		image smallImage = imread(pathToSmallImg);
 		const size_t h_small = smallImage.size();
@@ -133,97 +142,80 @@ public:
 		}
 	}
 
-	bool operator()(image &result) {
+	bool operator()(rect &result) {
 		if (mRects.empty())
 			return false;
 
 		rect currRect = mRects.front();
 		mRects.pop();
-		result = getRegionImg(mBigImage, currRect);
-//		cout << "imageSlicer" << currRect.topLeftX << " " << currRect.topLeftY << endl;
+//		if (currRect == rect(0,0,0,0)) {
+//			cout << "WTF ??!!!";
+//		}
+
+		result = currRect;
+//		cout << result;
 		return true;
-	}
-};
-
-class smallImageGenerator {
-	image mImage;
-
-public:
-	smallImageGenerator(const string &pathToSmallImg) : mImage(imread(pathToSmallImg)) {}
-
-	bool operator()(image &result) {
-		result = mImage;
-//		cout << "smallImageGenerator" << endl;
-		return true;
-	}
-};
-
-class minHandler {
-	int64_t min = std::numeric_limits<int64_t>::max();
-	image curImage;
-
-public:
-	void operator()(std::tuple<image,int64_t> t) {
-		std::cout << "minHandler" << min << " ";
-		int64_t testMin = std::get<1>(t);
-		if (testMin < min) {
-			min = testMin;
-			curImage = std::get<0>(t);
-		}
-
-		std::cout << min << std::endl;
-	}
-
-	image getImage() {
-		return curImage;
 	}
 };
 
 void handleSmallImg(const string& smallImgPath, const string& resSmallImgPath) {
 	graph flowGraph;
-	source_node<image> imageToRegions(flowGraph, imageSlicer(smallImgPath), false);
-	source_node<image> smallImgGenerator(flowGraph, smallImageGenerator(smallImgPath), false);
-	buffer_node<image> regionsBuffer(flowGraph);
-	buffer_node<image> smallImagesBuffer(flowGraph);
-	join_node<tbb::flow::tuple<image,image>> joinNode(flowGraph);
-	function_node<tbb::flow::tuple<image,image>, std::tuple<image,int64_t>> worker(flowGraph, unlimited,
-		[](tbb::flow::tuple<image,image> imgPair) {
-			image first = (vector<vector<pixel>> &&) std::get<0>(imgPair);
-			image second = (vector<vector<pixel>> &&) std::get<1>(imgPair);
+
+	image mBigImage = imread(pathToBigImg);
+	image mSmallImage = imread(smallImgPath);
+
+	source_node<rect> imageToRegions(flowGraph, imageSlicer(smallImgPath), false);
+	buffer_node<rect> regionsBuffer(flowGraph);
+	function_node<rect, std::tuple<rect,int64_t>> worker(flowGraph, unlimited,
+		[&](const rect& cr) {
 			int64_t difference = 0;
-			size_t h = first.size();
-			size_t w = first.front().size();
+			size_t h = mSmallImage.size();
+			size_t w = mSmallImage.front().size();
 			for (size_t i = 0; i < h; ++i) {
 				for (size_t j = 0; j < w; ++j) {
-					difference += differenceBetween(first[i][j], second[i][j]);
+					difference += differenceBetween(mBigImage[cr.topLeftH + i][cr.topLeftW + j], mSmallImage[i][j]);
 				}
 			}
 
-//			cout << "worker" << difference;
+//			cout << cr << difference << endl;
 
-			return std::make_tuple(first, difference);
+//			if (cr.topLeftH == 735 && cr.topLeftW == 1508) {
+//				cout << difference << " " << std::numeric_limits<int64_t>::max() << " " << cr;
+//			}
+
+			return std::make_tuple(cr, difference);
 		}
 	);
 
-	buffer_node<std::tuple<image,int64_t>> resultBuffer(flowGraph);
-	minHandler mMinHandler;
-	function_node<std::tuple<image,int64_t>> worker2(flowGraph, 1, mMinHandler);
+	buffer_node<std::tuple<rect,int64_t>> resultBuffer(flowGraph);
+	int64_t min = std::numeric_limits<int64_t>::max();
+	rect mCurMinRect;
+	function_node<std::tuple<rect,int64_t>> worker2(flowGraph, 1, [&](std::tuple<rect,int64_t> t) {
+		rect &testRect = std::get<0>(t);
+		int64_t testMin = std::get<1>(t);
+		if (testMin < min) {
+			min = testMin;
+			mCurMinRect = testRect;
+		}
+
+//		if (testRect.topLeftH == 735 && testRect.topLeftW == 1508) {
+//			cout << min << " " << std::numeric_limits<int64_t>::max() << " " << mCurMinRect;
+//		}
+	});
 
 	make_edge(imageToRegions, regionsBuffer);
-	make_edge(smallImgGenerator, smallImagesBuffer);
-	make_edge(regionsBuffer, input_port<0>(joinNode));
-	make_edge(smallImagesBuffer, input_port<1>(joinNode));
-	make_edge(joinNode, worker);
+	make_edge(regionsBuffer, worker);
 	make_edge(worker, resultBuffer);
 	make_edge(resultBuffer, worker2);
 
 	imageToRegions.activate();
-	smallImgGenerator.activate();
 	flowGraph.wait_for_all();
 
-	imwrite(mMinHandler.getImage(), resSmallImgPath);
+	imwrite(getRegionImg(mBigImage, mCurMinRect), resSmallImgPath);
 }
 
 int main() {
 	handleSmallImg("data/cheer.dat","data/res_cheer.dat");
+	handleSmallImg("data/hat.dat","data/res_hat.dat");
+	handleSmallImg("data/chicken.dat","data/res_chicken.dat");
 }
