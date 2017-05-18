0813f00c5ebe91b4d4f8d5b553876c8c35fa0107
Lena Volzhina <lenavolzhina@ya.ru>
2017-05-18 17:04:15 +0300

    joined read_image with generate_subimages (not working)

--- csc/2017/3.TBBFlowGraph/veg/flow.cpp
+++ csc/2017/3.TBBFlowGraph/veg/flow.cpp
@@ -9,6 +9,8 @@
 using namespace std;
 using namespace tbb::flow;
 
+typedef tuple<int, int> coord;
+
 struct pixel {
     uint8_t r;
     uint8_t g;
@@ -74,30 +76,43 @@ int main() {
 
     graph g;
 
-    // 1. Узел, принимающий путь к файлу с маленьким изображением и передающий матрицу изображения
+    // 1a. Узел, принимающий путь к файлу с маленьким изображением и передающий матрицу изображения
     function_node<string, image> read_image(g, 1, [](string path) {
-        cout << "Processing " << path << endl;
+        cout << "(read_image) Processing " << path << endl;
         image im = imread(path);
-        cout << "Image size: " << im.size() << " x " << im[0].size() << endl;
+        cout << "(read_image) Image size: " << im.size() << " x " << im[0].size() << endl;
         return im;
     });
 
-    // 2. Узел, принимающий матрицу и генерирующий из большого изображения все возможные прямоугольники размера искомого изображения
-    function_node<image, image> generate_subimages(g, unlimited, [&](image small_image, int step=0) {
-        int n_rows = small_image.size(), n_cols = small_image[0].size();
-        int row_step = big_n_cols - n_cols + 1;
-        int cur_row = step / row_step, cur_col = step % row_step;
+    // 1b. Узел, хранящий матрицу маленького изображения для последующего использования
+    write_once_node<image> read_image_buffer(g);
 
-        cout << "Got step " << step << ", cur row = " << cur_row << ", cur col = " << cur_col << endl;
-        return small_image;  // TODO
-    });
+    // 2. Узел, генерирующий из большого изображения все возможные прямоугольники размера искомого изображения
+    int cur_step = 0;
+    source_node<coord> generate_subimages(g, [&]( coord &xy ) {
+        if (cur_step < big_n_rows * big_n_cols - 1) {
+            int step = ++cur_step;
+            xy = coord(step / big_n_rows, step % big_n_rows);
+            // cout << "(generate_subimages) " << step << endl;
+            return true;
+        } else {
+            return false;
+        }
+    }, false);
 
     // 3. Буферный узел
-    buffer_node<image> subimages_buffer(g);
+    buffer_node<coord> subimages_buffer(g);
 
 
-    // 4. Узел, подсчитывающий разницу между искомым изображением и кандидатом
+    // 4a. Узел, объединяющий данные из 1b и 3
+    join_node<tuple<image, coord>, queueing> join_buffers(g);
 
+    // 4b. Узел, подсчитывающий разницу между искомым изображением и кандидатом
+    function_node<tuple<image, coord>, tuple<int, coord>> diff_node(g, 1, [&](tuple<image, coord> im_and_xy) {
+        image im = get<0>(im_and_xy); coord xy = get<1>(im_and_xy);
+        // cout << "(diff_node) " << get<0>(xy) << "  " << get<1>(xy) << endl;
+        return tuple<int, coord>(123, xy);
+    });
 
     // 5. Узел, содержащий результат - минимальную разницу и координаты верхнего левого угла.
 
@@ -106,13 +121,19 @@ int main() {
 
 
     // Соединяем вершины ребрами
-    make_edge(read_image, generate_subimages);
+    make_edge(read_image, read_image_buffer);
     make_edge(generate_subimages, subimages_buffer);
+
+    make_edge(read_image_buffer, input_port<0>(join_buffers));
+    make_edge(subimages_buffer, input_port<1>(join_buffers));
+
+    make_edge(join_buffers, diff_node);
 
 
     // Запускаем
     string default_path = "/home/user/Documents/study/parallel/hpcourse/csc/2017/3.TBBFlowGraph/data/cheer.dat";
     read_image.try_put(default_path);
+    generate_subimages.activate();
 
     g.wait_for_all();
 }
