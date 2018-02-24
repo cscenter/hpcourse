fcb478c5342152534ec9a598019b129db0029610
Eduard Eduardov <eduard.eduardov@almworks.com>
2018-02-24 13:52:18 +0300

    Added 2018 and first task

---
+++ csc/2018/1.Pthread/Template.cpp
@@ -0,0 +1,34 @@
+#include <pthread.h>  
+#include <iostream>
+
+void* producer_routine(void* arg) {
+    // Wait for consumer to start
+
+    // Read data, loop through each value and update the value, notify consumer, wait for consumer to process
+
+}
+
+void* consumer_routine(void* arg) {
+    // notify about start
+    // allocate value for result
+    // for every update issued by producer, read the value and add to sum
+    // return pointer to result
+}
+
+void* consumer_interruptor_routine(void* arg) {
+    // wait for consumer to start
+
+    // interrupt consumer while producer is running
+}
+
+int run_threads() {
+    // start 3 threads and wait until they're done
+    // return sum of update values seen by consumer
+
+    return 0;
+}
+
+int main() {
+    std::cout << run_threads() << std::endl;
+    return 0;
+}
