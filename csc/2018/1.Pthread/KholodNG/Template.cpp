d5a4943d359820c0c079c22839461d3a18f79a25
nick0kcin <nikkholod@mail.ru>
2018-03-01 22:46:02 +0300

    fixed

--- csc/2018/1.Pthread/KholodNG/Template.cpp
+++ csc/2018/1.Pthread/KholodNG/Template.cpp
@@ -23,24 +23,23 @@ using namespace std;
  * 
  */
 volatile int shared_var=0;
-volatile bool owner=0; 
+volatile bool owner=false; 
 // owner =0 - shared_var должен обрабатывать producer
 // owner =1 - shared_var должен обрабатывать consumer
-volatile bool exit_state=0;
+volatile bool exit_state=false;
 pthread_mutex_t mutex1= PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex2= PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond1=PTHREAD_COND_INITIALIZER;
 pthread_cond_t cond2=PTHREAD_COND_INITIALIZER;
 
 void* producer_routine(void* arg){
-    std::vector<int> data;
     //получение данных, символ конца потока - ctrl+d
-    std::copy(std::istream_iterator<int>(std::cin),std::istream_iterator<int>(),std::back_inserter(data));
+    std::vector<int> data(std::istream_iterator<int>(std::cin),std::istream_iterator<int>());
     for(int i=0;i<data.size();++i){
         //работа с данными
         pthread_mutex_lock(&mutex1);
         shared_var=data[i];
-        owner=1;
+        owner=true;
         exit_state=(i==int(data.size())-1);
         pthread_cond_signal(&cond1);
         pthread_mutex_unlock(&mutex1);
@@ -57,11 +56,13 @@ void* producer_routine(void* arg){
 void* consumer_routine(void* arg){
     pthread_setcancelstate(PTHREAD_CANCEL_DISABLE,0);
     int result=0;
-    bool exit=0;
+    bool exit=false;
     while(!exit){
         pthread_mutex_lock(&mutex1);
         //ожидание результатов producer потока
-        pthread_cond_wait(&cond1,&mutex1);
+        while(!owner){
+            pthread_cond_wait(&cond1,&mutex1);
+        }
         //работа с данными
         result+=shared_var;
         exit=exit_state;
@@ -69,7 +70,7 @@ void* consumer_routine(void* arg){
         
         //информирование об окончании работы с данными
         pthread_mutex_lock(&mutex2);
-        owner=0;
+        owner=false;
         pthread_cond_broadcast(&cond2);
         pthread_mutex_unlock(&mutex2);
     }
@@ -78,7 +79,7 @@ void* consumer_routine(void* arg){
 
 void* consumer_interuptor_routine(void* arg){
     pthread_t* victim_id=(pthread_t*) arg;
-    bool exit=0;
+    bool exit=false;
     while(!exit){
         // попытки остановки потока
         for(int i=0;i<1000;++i)
@@ -93,12 +94,6 @@ void* consumer_interuptor_routine(void* arg){
 
 int run_threads(){
     int* res=0;
-    
-    //инциализация примитивов
-    pthread_mutex_init(&mutex1,NULL);
-    pthread_mutex_init(&mutex2,NULL);
-    pthread_cond_init(&cond1,NULL);
-    pthread_cond_init(&cond2,NULL);
     
     pthread_t producer,consumer,interruptor;
     
