06eea85b546331f15b6a8a38946d3fb0ef9be6de
bzik <alexander.bzikadze@gmail.com>
2018-03-01 20:40:30 +0300

    1.Pthread 1.1

--- csc/2018/1.Pthread/BzikadzeAV/main.cpp
+++ csc/2018/1.Pthread/BzikadzeAV/main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
 namespace details {
 
 // Class to provide communication between threads of the production-consumer problem
@@ -10,6 +12,7 @@ class Thread_communicator {
 public:
         Thread_communicator(const Thread_communicator& root) = delete;
         Thread_communicator& operator=(const Thread_communicator&) = delete;
+        ~Thread_communicator();
 
         // Thread_communicator is a singleton - one for all routines
         // As stated [here](https://stackoverflow.com/a/12249115) it is a thead-safe version
@@ -18,40 +21,46 @@ public:
             return instance;
         }
 
-        // Active wait for consumer to start
-        // It is supposed that consumer will start pretty fast
-        void wait_for_consumer() const;
+        // Thread wait for consumer to start
+        void wait_for_consumer();
 
         // Ends any active wait for consumer
         // Supposed to be ran only by consumer function
         void consumer_notify_start();
 
         // Updates contained value:
-        // - Locks _value_mutex
+        // - Expects locked _value_mutex
         // - Waits for verification that consumer got previous data 
-        // (waits for Production to become an active thread)
+        // (waits for Production to become an active thread), unlocks _value_mutex
+        // and locks _value_mutex after verification given
         // - Changes contained value
         // - If there are no data to come, the state is changed at the moment
-        // - Sets Consumation as an active thread
-        // - _value_mutex is unlocked
+        // - Sets Consumation as an active thread and releases _value_mutex
         void update(int value, bool new_data_status = true);
 
-        // Gets contained value:
-        // - Locks _value_mutex
+        // Gets contained value and sets data_status:
+        // - Expects locked _value_mutex
         // - Waits for verification that production sent previous data 
-        // (waits for Consumer to become an active thread)
+        // (waits for Consumer to become an active thread), unlocks _value_mutex
+        // and locks _value_mutex after verification given
         // - Gets contained value
-        // - Sets Production as an active thread
-        // - _value_mutex is unlocked
+        // - Sets Production as an active thread and releases _value_mutex
         int get_value();
 
-        // Returns if there are data left to consume
-        bool get_data_status() const;
+        // Thread-safely returns if there are data left to consume
+        bool get_data_status();
+
+        void lock_value();
+        void unlock_value();
 
 private:        
         Thread_communicator();
 
+        pthread_mutex_t _consumer_wait_mutex;
+        pthread_cond_t _consumer_wait_cond;
+
         pthread_mutex_t _value_mutex;
+        pthread_mutex_t _data_status_mutex;
 
         enum class Thread_type {
             Production,
@@ -60,6 +69,7 @@ private:
 
         struct Thread_state {
             Thread_state();
+            ~Thread_state();
             
             Thread_type active_thread;
             pthread_cond_t cond;
@@ -70,11 +80,11 @@ private:
             template <Thread_type thread>
             void change_state();
 
-        } _current_thread_state;
+        } _value_thread_state;
 
         bool _consumer_start = false;
         bool _there_is_data_to_come = true;
-        int _value;
+        int _value = 0;
 };
 
 }
@@ -93,12 +103,15 @@ void* producer_routine(void* arg) {
     for (int data; std::cin >> data; input_data.push_back(data));
 
     // Loop through each value and update the value, notify consumer, wait for consumer to process
+    inst().lock_value();
+
     for (auto data : input_data) {
         inst().update(data);
     }
-
     // Notify that production has resigned
     inst().update(0, false);
+
+    inst().unlock_value();
 
     pthread_exit(nullptr);
 }
@@ -116,9 +129,13 @@ void* consumer_routine(void* arg) {
 
     // Allocate value for result
     // For every update issued by producer, read the value and add to sum
+    inst().lock_value();
+
     while (inst().get_data_status()) {
         *sum += inst().get_value();
     }
+
+    inst().unlock_value();
 
     // return pointer to result
     pthread_exit(sum);
@@ -167,48 +184,65 @@ int main() {
 
 namespace details {
 
-void Thread_communicator::wait_for_consumer() const { 
-    while(!_consumer_start); 
+void Thread_communicator::wait_for_consumer() { 
+    pthread_mutex_lock(&_consumer_wait_mutex);
+    while(!_consumer_start) pthread_cond_wait(&_consumer_wait_cond, &_consumer_wait_mutex);
+    pthread_cond_broadcast(&_consumer_wait_cond);
+    pthread_mutex_unlock(&_consumer_wait_mutex);
 }
 
 void Thread_communicator::consumer_notify_start() {
+    pthread_mutex_lock(&_consumer_wait_mutex);
     _consumer_start = true;
+    pthread_cond_broadcast(&_consumer_wait_cond);
+    pthread_mutex_unlock(&_consumer_wait_mutex);
 }
 
 void Thread_communicator::update(int value, bool new_data_status) {
-    pthread_mutex_lock(&_value_mutex);
-    _current_thread_state.wait_untill_active<Thread_type::Production>(&_value_mutex);
+    _value_thread_state.wait_untill_active<Thread_type::Production>(&_value_mutex);
 
     _value = value;
-    _there_is_data_to_come = new_data_status;
+    if (!new_data_status) {
+        pthread_mutex_lock(&_data_status_mutex);
+        _there_is_data_to_come = new_data_status;
+        pthread_mutex_unlock(&_data_status_mutex);
+    }
 
-    _current_thread_state.change_state<Thread_type::Consumation>();
-    pthread_mutex_unlock(&_value_mutex);
+    _value_thread_state.change_state<Thread_type::Consumation>();
 }
 
 int Thread_communicator::get_value() {
-    pthread_mutex_lock(&_value_mutex);
-    _current_thread_state.wait_untill_active<Thread_type::Consumation>(&_value_mutex);
+    _value_thread_state.wait_untill_active<Thread_type::Consumation>(&_value_mutex);
 
     int value = _value;
 
-    _current_thread_state.change_state<Thread_type::Production>();
-    pthread_mutex_unlock(&_value_mutex);
-
+    _value_thread_state.change_state<Thread_type::Production>();
     return value;
-}
-
-bool Thread_communicator::get_data_status() const {
-    return _there_is_data_to_come;
 }
 
 Thread_communicator::Thread_communicator() {
     pthread_mutex_init(&_value_mutex, nullptr);
+    pthread_mutex_init(&_data_status_mutex, nullptr);
+
+    pthread_mutex_init(&_consumer_wait_mutex, nullptr);
+    pthread_cond_init(&_consumer_wait_cond, nullptr);
+}
+
+Thread_communicator::~Thread_communicator() {
+    pthread_mutex_destroy(&_value_mutex);
+    pthread_mutex_destroy(&_data_status_mutex);
+
+    pthread_mutex_destroy(&_consumer_wait_mutex);
+    pthread_cond_destroy(&_consumer_wait_cond);
 }
 
 Thread_communicator::Thread_state::Thread_state() {
     active_thread = Thread_type::Production;
     pthread_cond_init(&cond, nullptr);
+}
+
+Thread_communicator::Thread_state::~Thread_state() {
+    pthread_cond_destroy(&cond);
 }
 
 template <Thread_communicator::Thread_type thread>
@@ -220,6 +254,21 @@ template <Thread_communicator::Thread_type thread>
 void Thread_communicator::Thread_state::change_state() {
     active_thread = thread;
     pthread_cond_broadcast(&cond); // As we use one condition, we should notify them all?
+}
+
+void Thread_communicator::lock_value() {
+    pthread_mutex_lock(&_value_mutex);
+}
+
+void Thread_communicator::unlock_value() {
+    pthread_mutex_unlock(&_value_mutex);
+}
+
+bool Thread_communicator::get_data_status() {
+    pthread_mutex_lock(&_data_status_mutex);
+    auto res = _there_is_data_to_come;
+    pthread_mutex_unlock(&_data_status_mutex);
+    return res;
 }
 
 }
