c3269fb774c63d4c53633ba3c1a202efcfb7a370
Aleksandr Eslikov <aeslikov@ocs.ru>
2018-05-12 21:33:25 +0300

    Fixed remarks

--- csc/2018/2.LockFreeSet/EslikovAV/LockFreeSetImpl.java
+++ csc/2018/2.LockFreeSet/EslikovAV/LockFreeSetImpl.java
@@ -1,13 +1,13 @@
 import java.util.concurrent.atomic.AtomicMarkableReference;
 
 public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> {
-
+    
     private final Node head;
-
+    
     public LockFreeSetImpl() {
         this.head = new Node();
     }
-
+    
     @Override
     public boolean add(T value) {
         while (true) {
@@ -22,12 +22,12 @@ public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> 
             }
         }
     }
-
+    
     @Override
     public boolean remove(T value) {
         while (true) {
             Window window = find(value);
-            if (window.current == null || window.current.value == null || window.current.value.compareTo(value) != 0) {
+            if (window.current == null || window.current.value.compareTo(value) != 0) {
                 return false;
             } else {
                 Node successor = window.current.next.getReference();
@@ -40,43 +40,45 @@ public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> 
             }
         }
     }
-
+    
     @Override
     public boolean contains(T value) {
         Node current = head.next.getReference();
-
-        while (current != null && current.value != null
-                && current.value.compareTo(value) < 0) {
+        
+        while (current != null
+               && current.value.compareTo(value) < 0) {
             current = current.next.getReference();
         }
+        
         return current != null
-                && !current.next.isMarked()
-                && current.value != null
-                && current.value.compareTo(value) == 0;
+        && !current.next.isMarked()
+        && current.value.compareTo(value) == 0;
     }
-
+    
     @Override
     public boolean isEmpty() {
         return head.next.getReference() == null;
     }
-
+    
     private Window find(T value) {
-        retry:
         while (true) {
             Node previous = head;
             Node current = previous.next.getReference();
             Node successor;
-
+            
             while (true) {
+                if (current == null) {
+                    return new Window(previous, null);
+                }
                 successor = current.next.getReference();
                 if (current.next.isMarked()) {
                     if (!previous.next.compareAndSet(current, successor, false, false)) {
-                        continue retry;
+                        break;
                     }
                     current = successor;
-
+                    
                 } else {
-                    if (current.value == null || current.value.compareTo(value) > 0) {
+                    if (current.value.compareTo(value) > 0) {
                         return new Window(previous, current);
                     }
                     previous = current;
@@ -85,27 +87,27 @@ public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> 
             }
         }
     }
-
+    
     private final class Window {
         private final Node previous;
         private final Node current;
-
+        
         Window(Node previous, Node current) {
             this.previous = previous;
             this.current = current;
-
+            
         }
     }
-
+    
     private final class Node {
         private final T value;
         private final AtomicMarkableReference<Node> next;
-
+        
         Node() {
             this.value = null;
             this.next = new AtomicMarkableReference<>(null, false);
         }
-
+        
         Node(T value, Node next) {
             this.value = value;
             this.next = new AtomicMarkableReference<>(next, false);
