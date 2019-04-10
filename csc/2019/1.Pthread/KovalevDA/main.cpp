0424124010b203c40afcfa7064edebb037824691
Dmitry Kovalev <dmitry.kovalev.m@ya.ru>
2019-04-10 04:35:45 +0300

    Producer & consumer

---
+++ csc/2019/1.Pthread/KovalevDA/main.cpp
@@ -0,0 +1,160 @@
+#include <cstdlib>
+#include <unistd.h>
+#include <pthread.h>
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+#define NOERROR 0
+#define OVERFLOW 1
+
+int max_sleep_time;
+
+int storage = 0;
+bool storage_updated = false;
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t produced = PTHREAD_COND_INITIALIZER;
+pthread_cond_t consumed = PTHREAD_COND_INITIALIZER;
+
+bool producer_done = false;
+
+thread_local int error = NOERROR;
+
+int get_last_error() {
+    return error;
+}
+
+void set_last_error(int code) {
+    error = code;
+}
+
+// true if overflow occurred, false otherwise
+bool checked_add(int fst, int snd, int* r_sum) {
+    *r_sum = fst + snd;
+    return (fst > 0 && snd > 0 && *r_sum <= 0)
+            || (fst < 0 && snd < 0 && *r_sum >= 0);
+}
+
+struct Result {
+    int partial_sum;
+    int error;
+};
+
+Result* consumer_result(int sum) {
+    auto result = (Result*)(malloc(sizeof(Result)));
+    result->partial_sum = sum;
+    result->error = get_last_error();
+    return result;
+}
+
+void* producer_routine(void* arg) {
+    int* storage_arg = static_cast<int*>(arg);
+
+    int n;
+    while (cin >> n) {
+        pthread_mutex_lock(&mutex);
+
+        while (storage_updated) {
+            pthread_cond_wait(&consumed, &mutex);
+        }
+
+        *storage_arg = n;
+        storage_updated = true;
+        pthread_cond_signal(&produced);
+
+        pthread_mutex_unlock(&mutex);
+    }
+
+    // we need to wait until the last produced value is consumed
+    // then wake up all the waiting consumers and let them finished
+    pthread_mutex_lock(&mutex);
+
+    while (storage_updated) {
+        pthread_cond_wait(&consumed, &mutex);
+    }
+    producer_done = true;
+    storage_updated = true;
+    pthread_cond_broadcast(&produced);
+
+    pthread_mutex_unlock(&mutex);
+}
+
+void* consumer_routine(void* arg) {
+    int* storage_arg = static_cast<int*>(arg);
+    int sum = 0;
+
+    while (true) {
+        pthread_mutex_lock(&mutex);
+
+        while (!storage_updated) {
+            pthread_cond_wait(&produced, &mutex);
+        }
+
+        if (producer_done) {
+            pthread_mutex_unlock(&mutex);
+            pthread_exit(consumer_result(sum));
+        }
+
+        int current_sum;
+        if (checked_add(sum, *storage_arg, &current_sum)) {
+            set_last_error(OVERFLOW);
+
+            storage_updated = false;
+            pthread_cond_signal(&consumed);
+
+            pthread_mutex_unlock(&mutex);
+            pthread_exit(consumer_result(sum));
+        }
+        sum = current_sum;
+
+        storage_updated = false;
+        pthread_cond_signal(&consumed);
+
+        pthread_mutex_unlock(&mutex);
+
+        usleep(rand() % (max_sleep_time + 1) * 1000);
+    }
+}
+
+void* consumer_interruptor_routine(void* arg) {
+    // wait for consumers to start
+
+    // interrupt random consumer while producer is running
+}
+
+int run_threads(int count) {
+    int sum = 0;
+
+    // create producer thread
+    pthread_t producer;
+    pthread_create(&producer, nullptr, producer_routine, &storage);
+    // create consumer threads
+    pthread_t consumers[count];
+    for (int i = 0; i < count; i++) {
+        pthread_create(&consumers[i], nullptr, consumer_routine, &storage);
+    }
+
+    // wait until producer and consumers finish the work
+    // if one of the consumers returns OVERFLOW we don't wait for others
+    pthread_join(producer, nullptr);
+    for (int i = 0; i < count; i++) {
+        Result* result;
+        pthread_join(consumers[i], (void**) &result);
+        if (result->error == OVERFLOW || checked_add(sum, result->partial_sum, &sum)) {
+            cout << "overflow";
+            return 1;
+        }
+    }
+
+    cout << sum << endl;
+    return 0;
+}
+
+int main(int argc, char** argv) {
+    int consumers_count = atoi(argv[1]);
+    max_sleep_time = atoi(argv[2]);
+
+    return run_threads(consumers_count);
+}
