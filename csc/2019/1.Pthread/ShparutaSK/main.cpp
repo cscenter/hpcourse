e21a602463758243130a6ed26ad714c6eb5a52a1
shparutask <sofish7184@gmail.com>
2019-04-19 00:32:18 +0300

    correctings cycles, interruptor and returning errors and values

--- csc/2019/1.Pthread/ShparutaSK/main.cpp
+++ csc/2019/1.Pthread/ShparutaSK/main.cpp
@@ -4,40 +4,45 @@
 #include <vector>
 #include <iostream>
 #include <time.h>
+#include <string>
+#include <limits.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <chrono>
+#include <windows.h>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
-//#define NO_ERROR 0 
 #define OVERFLOW 1
-#define MAX_INT 2147483647
+
+struct Result {
+	int sum;
+	int error;
+};
 
 thread_local int tls_error = 0;
 
-void sleepcp(int milliseconds) // Cross-platform sleep function
-{
-	clock_t time_end;
-	time_end = clock() + milliseconds * CLOCKS_PER_SEC / 1000;
-	while (clock() < time_end);
-}
-
-typedef struct {
-	int value; // the buffer
-	bool was_consumed = false;
-	pthread_mutex_t mutex_read = PTHREAD_MUTEX_INITIALIZER;
-	pthread_mutex_t mutex_write = PTHREAD_MUTEX_INITIALIZER;
-} buffer_t;
-
-int N;
-int max_time_milliseconds_sleep;
-vector<buffer_t> sharedata_thread;
+int N = 0;
+int max_time_milliseconds_sleep = 0;
+int sharedata_thread;
+int actual_consumers_count = 0;
 
 vector<pthread_t> consumers;
 pthread_t producer;
 pthread_t interruptor;
 
-pthread_cond_t condition_consumer_launch = PTHREAD_COND_INITIALIZER;
-pthread_mutex_t mutex_interrupt = PTHREAD_MUTEX_INITIALIZER;
-bool consumer_launch = false;
+bool consumer_started = false;
+bool producer_running = false;
+bool data_read = false;
+bool consumer_procceed = false;
+
+pthread_mutex_t mutex_consumers_counter = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_common = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t producer_consumer_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t consumer_producer_mutex = PTHREAD_MUTEX_INITIALIZER;
+
 
 int get_last_error() {
 	// return per-thread error code
@@ -52,74 +57,93 @@ void set_last_error(int code) {
 
 
 void* producer_routine(void* arg) {
+	producer_running = true;
+
+	// wait for consumer to start
+	while (!consumer_started);
+
 	// read data, loop through each value and update the value, notify consumer, wait for consumer to process 
-	char c = ' ';
-	while (c != '\n')
+	while (cin >> sharedata_thread)
 	{
-		buffer_t buffer;
-		cin >> buffer.value;
-		sharedata_thread.push_back(buffer);
-		c = getchar();
+		pthread_mutex_lock(&producer_consumer_mutex);
+		data_read = true;
+		pthread_mutex_unlock(&producer_consumer_mutex);
+
+		pthread_mutex_lock(&consumer_producer_mutex );
+		
+		while (!consumer_procceed);
+		consumer_procceed = false;
+		
+		pthread_mutex_unlock(&consumer_producer_mutex );
 	}
 
-	for (int i = 0; i < sharedata_thread.size(); i++)
-	{
-		pthread_mutex_lock(&sharedata_thread[i].mutex_write);
-		sharedata_thread[i].value++;
-		pthread_mutex_unlock(&sharedata_thread[i].mutex_write);
-	}
+	producer_running = false;
 
 	return NULL;
 }
 
 
 void* consumer_routine(void* arg) {
-	int p = 0;
-
+	// notify about start
 	pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
-	pthread_cond_broadcast(&condition_consumer_launch);
-	consumer_launch = true;
+	consumer_started = true;
 
-	// for every update issued by producer, read the value and add to sum
-	for (int i = 0; i < sharedata_thread.size(); i++)
+	pthread_mutex_lock(&mutex_consumers_counter);
+	actual_consumers_count++;
+	pthread_mutex_unlock(&mutex_consumers_counter);
+
+	// for every update issued by producer, read the value and add to sum	
+	int local_sum = 0;
+	while (true)
 	{
-		pthread_mutex_lock(&sharedata_thread[i].mutex_read);
-		if (!sharedata_thread[i].was_consumed)
-		{
-			pthread_mutex_lock(&sharedata_thread[i].mutex_write);
-			sharedata_thread[i].was_consumed = true;
+		pthread_mutex_lock(&producer_consumer_mutex);
+		while (!data_read && producer_running);
 
-			if (*(int*)arg + sharedata_thread[i].value >= MAX_INT)
-			{
+		if (!producer_running) {
+			pthread_mutex_unlock(&producer_consumer_mutex);
+			break;
+		}
+
+		if (data_read) {
+			int local_data = *(int*)arg;
+			data_read = false;
+			pthread_mutex_unlock(&producer_consumer_mutex);
+
+			pthread_mutex_lock(&consumer_producer_mutex);
+			consumer_procceed = true;
+			pthread_mutex_unlock(&consumer_producer_mutex);
+
+			if ((local_sum > 0 && local_data > INT_MAX - local_sum) || (local_sum < 0 && local_data < INT_MIN - local_sum)) {
 				set_last_error(OVERFLOW);
-				printf("overflow\n");
-				pthread_mutex_unlock(&sharedata_thread[i].mutex_write);
-				pthread_mutex_unlock(&sharedata_thread[i].mutex_read);
-				system("pause");
-				exit(OVERFLOW);
+				break;
 			}
 
-			*(int*)arg += sharedata_thread[i].value;
-			sleepcp(rand() % max_time_milliseconds_sleep);
-			pthread_mutex_unlock(&sharedata_thread[i].mutex_write);
+			local_sum += local_data;
+
+			Sleep(rand() % max_time_milliseconds_sleep);
 		}
-		pthread_mutex_unlock(&sharedata_thread[i].mutex_read);
+
 	}
-	return(&tls_error);
+
+	pthread_mutex_lock(&mutex_consumers_counter);
+	actual_consumers_count--;
+	pthread_mutex_unlock(&mutex_consumers_counter);
+
+	// return pointer to result (for particular consumer)
+	Result *res = new Result;
+	res->sum = local_sum;
+	res->error = get_last_error();
+	return (void *)res;
 }
 
 
 void* consumer_interruptor_routine(void* arg) {
 	// wait for consumers to start
-	pthread_mutex_lock(&mutex_interrupt);
-
-	while (!consumer_launch)
-		pthread_cond_wait(&condition_consumer_launch, &mutex_interrupt);
-
-	pthread_mutex_unlock(&mutex_interrupt);
+	while (!consumer_started);
 
 	// interrupt random consumer while producer is running 
-	pthread_cancel(*(pthread_t *)arg);
+	while (actual_consumers_count > 0)
+		pthread_cancel(consumers[rand() % actual_consumers_count - 1]);
 
 	return NULL;
 }
@@ -127,38 +151,46 @@ void* consumer_interruptor_routine(void* arg) {
 
 int run_threads() {
 	int sum = 0;
-	int error_code = 0;
-
-	//read count of consumers, create a vector
-	cin >> N;
-	cin >> max_time_milliseconds_sleep;
-	getchar();
+	actual_consumers_count = 0;
 
 	consumers = vector<pthread_t>(N);
 
 	pthread_create(&producer, NULL, &producer_routine, NULL);
-	pthread_join(producer, NULL);
-
-	int num_thread_for_interrupt = rand() % N - 1;
-	pthread_create(&interruptor, NULL, &consumer_interruptor_routine, &consumers[num_thread_for_interrupt]);
+	pthread_create(&interruptor, NULL, &consumer_interruptor_routine, NULL);
 
 	// start N threads and wait until they're done
 	for (int i = 0; i < N; i++)
-		pthread_create(&consumers[i], NULL, &consumer_routine, &sum);
+		pthread_create(&consumers[i], NULL, &consumer_routine, &sharedata_thread);
 
-	for (int i = 0; i < N; i++)
-		int r = pthread_join(consumers[i], NULL);
-
+	pthread_join(producer, NULL);
 	pthread_join(interruptor, NULL);
+
+	for (int i = 0; i < N; i++) {
+		Result* res;
+		pthread_join(consumers[i], (void **)&res);
+
+		if (res->error == OVERFLOW || (res->sum > 0 && sum > INT_MAX - res->sum) || (res->sum < 0 && sum < INT_MIN - res->sum))
+			set_last_error(OVERFLOW);
+
+		if (get_last_error() != OVERFLOW)
+			sum += res->sum;
+		delete(res);
+	}
+
+
+	if (get_last_error() == OVERFLOW) {
+		std::cout << "overflow" << std::endl;
+		return 1;
+	}
 
 	// return aggregated sum of values
 	std::cout << sum << std::endl;
-
-	system("pause");
-	return error_code;
+	return 0;
 }
 
 
-int main() {
+int main(int argc, char *argv[]) {
+	N = (int)argv[1];
+	max_time_milliseconds_sleep = (int)argv[2];
 	return run_threads();
 }
