d0bff2453ccfe6f513f56738303c2ebf44fa176c
fresheed <ok.john.rus@gmail.com>
2019-05-13 03:12:46 +0300

    failing on given tests

--- csc/2019/2.LockFreeSet/LockFreeSetImpl.java
+++ csc/2019/2.LockFreeSet/LockFreeSetImpl.java
@@ -11,9 +11,11 @@ import static java.util.stream.Collectors.toList;
 public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> {
     final private ItemWrapper<T> root;
     private AtomicInteger insertsCounter = new AtomicInteger(0);
+    final private AtomicReference<SnapCollector<T>> snapCollector;
 
     public LockFreeSetImpl(){
         root=new ItemWrapper<>(null, new Info<T>(true, null)); // root value doesn't matter since it is never used
+        snapCollector=new AtomicReference<>(new SnapCollector<T>(false));
     }
 
     @Override
@@ -22,6 +24,7 @@ public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> 
             Slot<T> slot = findSlot(key);
             // slot represents candidate position to insert valid at some point of time
             if (slot.second!=null && slot.second.item.equals(key)) { // already presented - skip
+                snapCollector.get().reportInsert(slot.second);
                 return false;
             } else {
                 ItemWrapper newNode = new ItemWrapper(key, new Info<T>(true, slot.second));
@@ -29,6 +32,7 @@ public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> 
                 boolean inserted = slot.first.CASinfo(true, slot.second, replacement);
                 if (inserted) {
                     insertsCounter.incrementAndGet();
+                    snapCollector.get().reportInsert(newNode);
                     return true;
                 }
             }
@@ -48,6 +52,7 @@ public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> 
                 boolean result = slot.second.CASinfo(true, succ, replacement);
                 //System.out.println("Removed "+key+"?: "+result);
                 if (result) {
+                    snapCollector.get().reportDelete(slot.second);
                     // also try to actually remove; not affects linearization
                     slot.first.CASinfo(true, slot.second, new Info<T>(true, succ));
                     return true;
@@ -103,6 +108,13 @@ public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> 
         }
         // ? an error was here: should re-read cur.info before return ?
         boolean found=cur.item.equals(key) && cur.info.get().isPresented;
+        if (found){
+            if (info.isPresented){
+                snapCollector.get().reportInsert(cur);
+            } else {
+                snapCollector.get().reportDelete(cur);
+            }
+        }
         return found;
     }
 
@@ -129,7 +141,16 @@ public class LockFreeSetImpl<T extends Comparable<T>> implements LockFreeSet<T> 
 
     @Override
     public Iterator<T> iterator() {
-        throw new RuntimeException("debug");
+        updateCollector();
+        List<T> snapshot = snapCollector.get().collectSnapshot(root);
+        return snapshot.iterator();
+    }
+
+    private void updateCollector() {
+        SnapCollector collector = snapCollector.get();
+        if (!collector.isActive()){
+            snapCollector.compareAndSet(collector, new SnapCollector<T>(true));
+        } // otherwise participate in current snapshot
     }
 
 }
@@ -189,3 +210,211 @@ class Slot<T extends Comparable<T>> {
     }
 }
 
+class Report<T extends Comparable<T>> implements Comparable<Report<T>> {
+    @Override
+    public int compareTo(Report<T> o) {
+        return item.compareTo(o.item);
+    }
+
+    enum ReportType {
+        INSERT, DELETE
+    }
+    public final T item;
+    public final ReportType reportType;
+
+    Report(T item, ReportType reportType){
+        this.item=item;
+        this.reportType=reportType;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("(%s, %s)", (reportType==ReportType.INSERT ? "I": "D"),
+                (item == null ? "X" : item).toString());
+    }
+}
+
+class SnapCollector<T extends Comparable<T>> {
+
+    // volatile since some threads may try to report after deactivate() and don't see isActive's new value
+    volatile private boolean isActive;
+    private GrowingQueue<GrowingQueue<Report<T>>> allThreadsReports = new GrowingQueue<>(null);
+    private ThreadLocal<GrowingQueue<Report<T>>> curThreadReports = new ThreadLocal<>();
+    private GrowingQueue<ItemWrapper<T>> additions = new GrowingQueue<>(null);
+
+    private final Report<T> DUMMY = new Report<T>(null, null);
+
+    SnapCollector(boolean initActive){
+        isActive=initActive;
+    }
+
+    private boolean setupThreadReports() {
+        if (curThreadReports.get() == null){
+            GrowingQueue<Report<T>> reports = new GrowingQueue<>(null);
+            boolean inserted = allThreadsReports.insert(reports); // lock-free, NOT wait-free
+            if (inserted) {
+                curThreadReports.set(reports);
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    public void reportDelete(ItemWrapper<T> item){
+        if (isActive && setupThreadReports()){
+            try {curThreadReports.get().tryInsert(new Report<T>(item.item, Report.ReportType.DELETE));}
+            catch (RuntimeException e) {}
+        }
+    }
+
+    public void reportInsert(ItemWrapper<T> item){
+        if (isActive && setupThreadReports()){
+            try {curThreadReports.get().tryInsert(new Report<T>(item.item, Report.ReportType.INSERT));}
+            catch (RuntimeException e) {}
+        }
+    }
+
+    private void blockReports(){
+        // no new local queues can be added now since deactivate() was called, so we can iterate big queue
+        ItemWrapper<GrowingQueue<Report<T>>> queue = allThreadsReports.head;
+        Info<GrowingQueue<Report<T>>> info = queue.info.get();
+        while (info.next != null) {
+            queue = info.next;
+            info = queue.info.get();
+            try {queue.item.tryInsert(queue.item.dummy);}
+            catch (RuntimeException e) {}
+        }
+    }
+
+    private void blockAdditions(){
+        additions.insert(additions.dummy);
+    }
+
+    public void reportFound(ItemWrapper<T> item){
+        if (isActive){
+            additions.insert(item);
+        }
+    }
+//
+    public void deactivate(){
+        isActive = false;
+    }
+
+    public boolean isActive(){
+        return isActive;
+    }
+
+    public List<T> collectSnapshot(ItemWrapper<T> root){
+        ItemWrapper<T> cur=root;
+        while (isActive){
+            Info<T> info = cur.info.get();
+            if (info.isPresented && cur!=root){
+                reportFound(cur);
+            }
+            if (info.next!=null){
+                cur=info.next;
+            } else {
+                deactivate(); // interrupts this loop in other collecting threads
+                blockAdditions();
+            }
+        }
+        blockReports();
+
+        Set<T> inserts = new HashSet<T>(), removals = new HashSet<T>();
+        ItemWrapper<GrowingQueue<Report<T>>> oneThreadReports = allThreadsReports.head;
+        Info<GrowingQueue<Report<T>>> threadReportsInfo = oneThreadReports.info.get();
+        while (threadReportsInfo.next != null && threadReportsInfo.next.item != null) {
+            oneThreadReports = threadReportsInfo.next;
+            threadReportsInfo = oneThreadReports.info.get();
+            ItemWrapper<Report<T>> report = oneThreadReports.item.head;
+            Info<Report<T>> reportInfo = report.info.get();
+            while (reportInfo.next != null && reportInfo.next.item != null) {
+                report = reportInfo.next;
+                reportInfo = report.info.get();
+                (report.item.reportType == Report.ReportType.INSERT ? inserts : removals).add(report.item.item);
+            }
+        }
+
+        ItemWrapper<ItemWrapper<T>> foundNode = additions.head;
+        Info<ItemWrapper<T>> nodeInfo = foundNode.info.get();
+        while (nodeInfo.next != null && nodeInfo.next.item != null) {
+            foundNode = nodeInfo.next;
+            nodeInfo = foundNode.info.get();
+            inserts.add(foundNode.item.item);
+        }
+
+        Set<T> snapshot=new HashSet<>();
+        snapshot.addAll(inserts);
+        snapshot.removeAll(removals);
+
+        List<T> listSnapshot=new ArrayList<T>(snapshot);
+        Collections.sort(listSnapshot);
+        return listSnapshot;
+    }
+}
+
+/* Implements a lock-free interface for insertions only
+ */
+class GrowingQueue<T extends Comparable<T>> implements Comparable<GrowingQueue<T>> {
+    public final ItemWrapper<T> head;
+    public AtomicReference<ItemWrapper<T>> tail;
+    public final T dummy;
+    private static int idc = 0;
+    private int id;
+
+    public GrowingQueue(T dummyItem){
+        head = new ItemWrapper<T>(null, new Info<T>(true, null));
+        tail = new AtomicReference<>(head);
+        dummy = dummyItem;
+        id = idc;
+        idc++;
+    }
+
+    public boolean insert(T item){
+        while (true) {
+            try {
+                if (tryInsert(item)){
+                    break;
+                }
+            } catch (RuntimeException e){ // don't try if dummy was detected
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean tryInsert(T item){
+        ItemWrapper<T> curLast = tail.get();
+        if (curLast == dummy) throw new RuntimeException("dummy inserted");
+        Info<T> curLastInfo = curLast.info.get();
+        if (curLastInfo.next == null) {
+            ItemWrapper<T> newLast = new ItemWrapper<T>(item, new Info<>(true, null));
+            Info<T> replacement = new Info<T>(true, newLast);
+            boolean added = curLast.CASinfo(true, null, replacement);
+            if (added){
+                boolean fixed = tail.compareAndSet(curLast, newLast);
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            Info<T> replacement = new Info<T>(true, curLastInfo.next);
+            tail.compareAndSet(curLast, curLastInfo.next);
+            return false;
+        }
+    }
+
+    // needed to organize queue of queues
+    @Override
+    public int compareTo(GrowingQueue<T> o) {
+        return new Integer(id).compareTo(new Integer(o.id));
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Q%d", id);
+    }
+}
--- csc/2019/2.LockFreeSet/LockFreeSetImplTest.java
+++ csc/2019/2.LockFreeSet/LockFreeSetImplTest.java
@@ -1,6 +1,7 @@
 import com.devexperts.dxlab.lincheck.LinChecker;
 import com.devexperts.dxlab.lincheck.LoggingLevel;
 import com.devexperts.dxlab.lincheck.Options;
+import com.devexperts.dxlab.lincheck.annotations.OpGroupConfig;
 import com.devexperts.dxlab.lincheck.annotations.Operation;
 import com.devexperts.dxlab.lincheck.annotations.Param;
 import com.devexperts.dxlab.lincheck.paramgen.IntGen;
@@ -11,50 +12,73 @@ import com.devexperts.dxlab.lincheck.verifier.linearizability.LinearizabilityVer
 
 import java.util.*;
 
-@Param(name = "key", gen = IntGen.class, conf = "1:2")
+@Param(name = "key", gen = IntGen.class, conf = "1:3")
 @StressCTest(verifier = LinearizabilityVerifier.class)
+@OpGroupConfig(name = "T1", nonParallel = true)
+@OpGroupConfig(name = "T2", nonParallel = true)
+@OpGroupConfig(name = "T3", nonParallel = true)
 public class LockFreeSetImplTest {
 
     private LockFreeSetImpl<Integer> set = new LockFreeSetImpl<>();
 
     public static void main(String[] args) {
-        //randomTests();
+        randomTests();
         lincheckTests();
     }
 
-    @Operation
-    public boolean add(@Param(name = "key") int key) {
-        return set.add(key);
+//    @Operation
+//    public boolean add(@Param(name = "key") int key) {
+//        return set.add(key);
+//    }
+//
+//    @Operation
+//    public boolean remove(@Param(name = "key") int key) {
+//        return set.remove(key);
+//    }
+//
+//    @Operation
+//    public boolean contains(@Param(name = "key") int key) {
+//        return set.contains(key);
+//    }
+//
+//    @Operation
+//    public boolean isEmpty() {
+//        return set.isEmpty();
+//    }
+//
+//    @Operation
+//    public int tryReadFirst() {
+//        return tryReadFirst(set.iterator());
+//    }
+
+    private int tryReadFirst(Iterator<Integer> iterator){
+        if (iterator.hasNext()){
+            return iterator.next();
+        } else {
+            return -1;
+        }
     }
 
-    @Operation
-    public boolean remove(@Param(name = "key") int key) {
-        return set.remove(key);
-    }
+    @Operation(runOnce = true, group="T1") public boolean o2(){ return set.isEmpty();}
+    @Operation(runOnce = true, group="T1") public boolean o3(){ return set.add(2);}
+    @Operation(runOnce = true, group="T1") public int o4(){ return tryReadFirst(set.iterator());}
 
-    @Operation
-    public boolean contains(@Param(name = "key") int key) {
-        return set.contains(key);
-    }
+    @Operation(runOnce = true, group="T2") public boolean o6(){ return set.add(2);}
+    @Operation(runOnce = true, group="T2") public int o7(){ return tryReadFirst(set.iterator());}
+    @Operation(runOnce = true, group="T2") public boolean o8(){ return set.contains(2);}
+    @Operation(runOnce = true, group="T2") public boolean o10(){ return set.contains(3);}
 
-    @Operation
-    public boolean isEmpty() {
-        return set.isEmpty();
-    }
+    @Operation(runOnce = true, group="T3") public boolean o11(){ return set.add(3);}
+    @Operation(runOnce = true, group="T3") public boolean o12(){ return set.remove(2);}
+    @Operation(runOnce = true, group="T3") public int o13(){ return tryReadFirst(set.iterator());}
 
-
-    /*@Operation
-    public  tryReadSecond() {
-        return set.remove(key);
-    }
-*/
 
     @org.junit.Test
     public static void lincheckTests() {
         Options opts = new StressOptions()
                 .iterations(5000)
                 .threads(3)
-                .invocationsPerIteration(10) // what does this parameter affect ?
+                .invocationsPerIteration(1000) // what does this parameter affect ?
                 .actorsPerThread(5)
                 .actorsBefore(0)
                 .actorsAfter(0)
