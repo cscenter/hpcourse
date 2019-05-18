28d1816d8e00e7522e2198aa03064fa937fbedf4
dm-kovalev <dmitry.kovalev.m@ya.ru>
2019-05-18 07:18:20 +0300

    Add iterator (lincheck tests sometimes work infinitely)

--- csc/2019/2.LockFreeSet/KovalevDA/LockFreeSetImplTest.java
+++ csc/2019/2.LockFreeSet/KovalevDA/LockFreeSetImplTest.java
@@ -3,10 +3,12 @@ import com.devexperts.dxlab.lincheck.annotations.Operation;
 import com.devexperts.dxlab.lincheck.strategy.stress.StressCTest;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @StressCTest
 public class LockFreeSetImplTest {
     private LockFreeSetImpl<Integer> set = new LockFreeSetImpl<>();
-
     @Operation
     public boolean add(int value) {
         return set.add(value);
@@ -25,6 +27,15 @@ public class LockFreeSetImplTest {
     @Operation
     public boolean isEmpty() {
         return set.isEmpty();
+    }
+
+    @Operation
+    public List<Integer> iterator() {
+        List<Integer> list = new ArrayList<>();
+        for (Integer i: set) {
+            list.add(i);
+        }
+        return list;
     }
 
     @Test
